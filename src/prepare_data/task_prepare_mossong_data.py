737d96d4fe629db79920cb967ed6cd5f0e43a1a0
Klara Roehrl <klara.roehrl@gmail.com>
2020-11-30 16:37:49 +0100

    Finish addressing review comments.

--- src/prepare_data/task_prepare_mossong_data.py
+++ src/prepare_data/task_prepare_mossong_data.py
@@ -55,31 +55,28 @@ def task_prepare_mossong_data(depends_on, produces):
     sday = _prepare_day_data(datasets["sday"])
 
     # contact_data
-    contacts = _build_contacts(
+    contacts = _merge_mossong_data(
         contacts=contacts, participants=participants, sday=sday, hh=hh
     )
+    contacts = _nice_columns_in_contact_data(contacts)
     contacts.to_pickle(produces["contact_data"])
 
     # household sample for initial states
-    with_missing_ages = _from_wide_to_long_format(hh)
-    only_complete_ages = _drop_hh_with_missing_ages(with_missing_ages)
-    german_hhs = only_complete_ages.query("country == 'DE_TOT'")
-    german_hhs.to_csv(produces["hh_sample"])
+    hh = hh.query("country == 'DE_TOT'")
+    hh = _from_wide_to_long_format(hh)
+    hh = _drop_hh_with_missing_ages(hh)
+    hh.to_csv(produces["hh_sample"])
 
     # household probability weights
-    german_hhs["collapsed_hh_size"] = german_hhs["hh_size"].where(
-        german_hhs["hh_size"] <= 5, pd.Interval(5.0, np.inf)
+    hh["collapsed_hh_size"] = hh["hh_size"].where(
+        hh["hh_size"] <= 5, pd.Interval(5.0, np.inf)
     )
-    sample_hh_size_shares = german_hhs["collapsed_hh_size"].value_counts(normalize=True)
+    sample_hh_size_shares = hh["collapsed_hh_size"].value_counts(normalize=True)
     inv_prob_weights = datasets["eu_hh_size_shares"]["DE_TOT"] / sample_hh_size_shares
-    german_hhs["hh_inv_prob_weights"] = german_hhs["collapsed_hh_size"].replace(
-        inv_prob_weights
-    )
-    german_hhs["probability"] = (
-        german_hhs["hh_inv_prob_weights"] / german_hhs["hh_inv_prob_weights"].sum()
-    )
-    hh_probabilities = german_hhs[["hh_id", "probability"]]
-    hh_probabilities.to_csv(produces["hh_probabilities"])
+    hh["hh_inv_prob_weights"] = hh["collapsed_hh_size"].replace(inv_prob_weights)
+    hh["probability"] = hh["hh_inv_prob_weights"] / hh["hh_inv_prob_weights"].sum()
+    hh_probs = hh[["hh_id", "probability"]]
+    hh_probs.to_csv(produces["hh_probabilities"])
 
 
 def _prepare_hh_data(common, extra):
@@ -119,20 +116,19 @@ def _prepare_participant_data(common, extra):
 
 
 def _prepare_contact_data(common):
-
-    contacts = common.copy(deep=True)
-    contacts["frequency"] = _make_frequencies_categorical(contacts["frequency_multi"])
-    contacts["phys_contact"].replace({1: True, 2: False}, inplace=True)
-    contacts["duration"] = _make_durations_categorical(contacts["duration_multi"])
+    df = common.copy(deep=True)
+    df["frequency"] = _make_frequencies_categorical(df["frequency_multi"])
+    df["phys_contact"].replace({1: True, 2: False}, inplace=True)
+    df["duration"] = _make_durations_categorical(df["duration_multi"])
 
     # the order of the location determines for contacts in more than one context to
     # which they are counted. This affects < 10% of contacts.
-    assert (contacts[LOCATIONS].sum(axis=1) > 1).mean() < 0.1
-    contacts["place"] = contacts.apply(_create_place, axis=1)
+    assert (df[LOCATIONS].sum(axis=1) > 1).mean() < 0.1
+    df["place"] = df.apply(_create_place, axis=1)
 
-    contacts = contacts.rename(columns={loc: loc[4:] for loc in LOCATIONS})
-    contacts.drop(columns=["frequency_multi", "duration_multi"], inplace=True)
-    return contacts
+    df = df.rename(columns={loc: loc[4:] for loc in LOCATIONS})
+    df.drop(columns=["frequency_multi", "duration_multi"], inplace=True)
+    return df
 
 
 def _make_frequencies_categorical(sr):
@@ -188,16 +184,45 @@ def _prepare_day_data(sday):
     return sday
 
 
-def _build_contacts(contacts, participants, hh, sday):
-    contacts = pd.merge(left=contacts, right=participants, on="part_id", validate="m:1")
-    contacts = pd.merge(left=contacts, right=hh, on="hh_id", validate="m:1")
-    contacts = pd.merge(left=contacts, right=sday, on="part_id", validate="m:1")
-    contacts.set_index("cont_id", inplace=True)
+def _merge_mossong_data(contacts, participants, hh, sday):
+    df = pd.merge(left=contacts, right=participants, on="part_id", validate="m:1")
+    df = pd.merge(left=df, right=hh, on="hh_id", validate="m:1")
+    df = pd.merge(left=df, right=sday, on="part_id", validate="m:1")
+    df.set_index("cont_id", inplace=True)
 
     # remove problematic entries
-    contacts = contacts[contacts["problems"] != "Y"]
+    df = df[df["problems"] != "Y"]
 
-    contacts = contacts.rename(
+    # remove one kid's work contact
+    df = df[~((df["part_age"] < 15) & (df["work"]))]
+    return df
+
+
+def _from_wide_to_long_format(hh):
+    """Convert the data from wide to long format."""
+    # To long format.
+    age_columns = [f"hh_age_{x}" for x in range(1, 21)]
+    hh = hh.melt(
+        id_vars=["hh_id", "country", "hh_size"],
+        value_vars=age_columns,
+        var_name="p_id",
+        value_name="age",
+    )
+
+    # Create personal id from the order in which ages were reported.
+    hh["p_id"] = hh["p_id"].str.split("_").str[-1].astype(np.uint8)
+
+    # Remove all observations which were artificially created in wide format.
+    hh = hh.loc[hh.p_id.le(hh.hh_size)]
+
+    hh = hh.astype({"hh_id": "category", "country": "category"})
+
+    return hh
+
+
+def _nice_columns_in_contact_data(df):
+    df = df.copy(deep=True)
+    df = df.rename(
         columns={
             "cnt_age_exact": "age_of_contact",
             "cnt_gender": "gender_of_contact",
@@ -224,54 +249,28 @@ def _build_contacts(contacts, participants, hh, sday):
         "type",
     ]
     drop_cols += [f"hh_age_{x}" for x in range(5, 21)]  # noqa
-    contacts.drop(columns=drop_cols, inplace=True)
+    df.drop(columns=drop_cols, inplace=True)
 
-    contacts["part_age_group"] = create_age_groups(contacts["part_age"])
-    contacts["age_group_of_contact"] = create_age_groups(contacts["age_of_contact"])
+    df["part_age_group"] = create_age_groups(df["part_age"])
+    df["age_group_of_contact"] = create_age_groups(df["age_of_contact"])
 
-    contacts["recurrent"] = contacts["frequency"].isin(
-        ["1-2 times a week", "(almost) daily"]
-    )
-
-    # remove one kid's work contact
-    contacts = contacts[~((contacts["part_age"] < 15) & (contacts["work"]))]
-    return contacts
+    df["recurrent"] = df["frequency"].isin(["1-2 times a week", "(almost) daily"])
+    return df
 
 
-def _from_wide_to_long_format(hh):
-    """Convert the data from wide to long format."""
-    # To long format.
-    age_columns = [f"hh_age_{x}" for x in range(1, 21)]
-    hh = hh.melt(
-        id_vars=["hh_id", "country", "hh_size"],
-        value_vars=age_columns,
-        var_name="p_id",
-        value_name="age",
-    )
-
-    # Create personal id from the order in which ages were reported.
-    hh["p_id"] = hh["p_id"].str.split("_").str[-1].astype(np.uint8)
-
-    # Remove all observations which were artificially created in wide format.
-    hh = hh.loc[hh.p_id.le(hh.hh_size)]
-
-    hh = hh.astype({"hh_id": "category", "country": "category"})
-
-    return hh
-
-
-def _drop_hh_with_missing_ages(hh):
+def _drop_hh_with_missing_ages(df):
     """Drop households that don't have ages for every person in the household."""
-    hh = hh.dropna()
+    df = df.copy(deep=True)
+    df = df.dropna()
 
     # Keep only complete households.
-    n_hh_members_with_age = hh.groupby("hh_id")["p_id"].transform("size")
-    hh = hh.loc[hh.hh_size.eq(n_hh_members_with_age)]
+    n_hh_members_with_age = df.groupby("hh_id")["p_id"].transform("size")
+    df = df.loc[df.hh_size.eq(n_hh_members_with_age)]
 
     # Drop households consisting of children only
-    oldest_above_16 = hh.groupby("hh_id")["age"].max() >= 16
+    oldest_above_16 = df.groupby("hh_id")["age"].max() >= 16
     adult_led_hh = oldest_above_16[oldest_above_16].index
-    hh = hh[hh["hh_id"].isin(adult_led_hh)]
+    df = df[df["hh_id"].isin(adult_led_hh)]
 
-    hh["age"] = hh["age"].astype(np.uint8)
-    return hh
+    df["age"] = df["age"].astype(np.uint8)
+    return df
