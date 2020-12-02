3f9cc21b3ccea813a6b11b2c6be54457da5e72d9
Klara Roehrl <klara.roehrl@gmail.com>
2020-12-02 19:31:13 +0100

    Add first get_{}_contact_model functions.

---
+++ src/contact_models/get_contact_models.py
@@ -0,0 +1,97 @@
+from functools import partial
+
+from src.create_contact_models import contact_model_functions as cm_funcs
+
+
+def get_household_contact_model():
+    household_contact_model = {
+        "households": {
+            "is_recurrent": True,
+            "model": cm_funcs.meet_hh_members,
+            "assort_by": ["hh_id"],
+            "loc": "households",
+        },
+    }
+    return household_contact_model
+
+
+# ----------------------------------------------------------------------------
+# Education Contact Models
+# ----------------------------------------------------------------------------
+
+
+def get_school_contact_model():
+    school_contact_model = {
+        "educ_school": {
+            "is_recurrent": True,
+            "model": partial(cm_funcs.attends_educational_facility, facility="school"),
+            "assort_by": ["school_class_id"],
+        }
+    }
+    return school_contact_model
+
+
+def get_preschool_contact_model():
+    preschool_contact_model = {
+        "educ_preschool": {
+            "is_recurrent": True,
+            "model": partial(
+                cm_funcs.attends_educational_facility, facility="preschool"
+            ),
+            "assort_by": ["preschool_group_id"],
+        }
+    }
+    return preschool_contact_model
+
+
+def get_nursery_contact_model():
+    nursery_contact_model = {
+        "educ_nursery": {
+            "is_recurrent": True,
+            "model": partial(cm_funcs.attends_educational_facility, facility="nursery"),
+            "assort_by": ["nursery_group_id"],
+        },
+    }
+    return nursery_contact_model
+
+
+# ----------------------------------------------------------------------------
+# Work Contact Models
+# ----------------------------------------------------------------------------
+
+
+def get_work_non_recurrent_contact_model():
+    work_non_recurrent_contact_model = {
+        "work_non_recurrent": {
+            "assort_by": ["age_group", "county"],
+            "is_recurrent": False,
+            "loc": "work_non_recurrent",
+            "model": partial(
+                cm_funcs.calculate_non_recurrent_contacts_from_empirical_distribution,
+                on_weekends=False,
+                query="occupation == 'working'",
+            ),
+        }
+    }
+    return work_non_recurrent_contact_model
+
+
+# ----------------------------------------------------------------------------
+# Other Contact Models
+# ----------------------------------------------------------------------------
+
+
+def get_other_non_recurrent_contact_model():
+    other_non_recurrent_contact_model = {
+        "other_non_recurrent": {
+            "is_recurrent": False,
+            "loc": "other_non_recurrent",
+            "assort_by": ["age_group", "county"],
+            "model": partial(
+                cm_funcs.calculate_non_recurrent_contacts_from_empirical_distribution,
+                on_weekends=True,
+                query=None,
+            ),
+        }
+    }
+    return other_non_recurrent_contact_model
