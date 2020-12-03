3159da1bbce529c631af6533c1d5a1ec483003f2
Klara Roehrl <klara.roehrl@gmail.com>
2020-12-03 18:17:18 +0100

    Add daily group ids. Add checks on the created background characteristics and daily group ids. Start adding weekly group ids.

---
+++ src/create_initial_states/add_weekly_ids.py
@@ -0,0 +1,175 @@
+import itertools as it
+
+import numba as nb
+import numpy as np
+import pandas as pd
+from sid.contacts import boolean_choice
+from sid.contacts import choose_other_group
+from sid.contacts import choose_other_individual
+from sid.contacts import create_group_indexer
+from sid.matching_probabilities import create_group_transition_probs
+from sid.shared import factorize_assortative_variables
+
+from src.shared import draw_from_distribution_for_subset
+
+
+def add_weekly_ids(
+    states, weekly_dist, seed, query, col_prefix, county_assortativeness
+):
+    """Add a column for every possible weekly contact.
+
+    We draw from the number of weekly contacts distribution in how
+    many weekly contact models a person participates and then randomly
+    choose in which column she'll be paired with someone. Lastly, for each
+    column we match people who participate in the respective contact model
+    with a specified geographic assortativeness.
+
+    Args:
+        states (pandas.DataFrame): sid states DataFrame
+        weekly_dist (pandas.Series): the index is the support of the
+            number of weekly contacts that are possible, the values are
+            the frequencies in the synthetic population we aim for of each
+            number of weekly contacts. One pair id column will be created
+            for every possible contact.
+        seed (int): seed.
+        query (str): query which subset of the population participates.
+            If None, everyone is grouped. (e.g. "occupation =='working'")
+        col_prefix (str): prefix for the columns to be created.
+        county_assortativeness (float): share of weekly contacts that
+            should belong to the same county.
+
+    Returns:
+        states (pandas.DataFrame): sit states DataFrame with additional columns
+            specifying the weekly work group pairs.
+
+    """
+    seed = it.count(seed)
+    weekly_ids = pd.DataFrame(index=states.index)
+    max_contacts = weekly_dist.index.max()
+
+    for i in range(max_contacts):
+        weekly_ids[f"{col_prefix}_{i}"] = -1
+
+    nr_of_weekly_contacts = draw_from_distribution_for_subset(
+        states=states,
+        distribution=weekly_dist,
+        query=query,
+        seed=next(seed),
+        outside_val=0,
+    ).to_numpy()
+
+    weekly_id_cols = []
+    for i in range(max_contacts):
+        col_name = f"{col_prefix}_{i}"
+        weekly_ids[col_name] = np.nan
+        weekly_id_cols.append(col_name)
+
+    weekly_ids[weekly_id_cols] = _create_pairs(
+        states=states,
+        nr_of_weekly_contacts=nr_of_weekly_contacts,
+        county_assortativeness=county_assortativeness,
+        seed=next(seed),
+    )
+    weekly_ids = weekly_ids.astype("category")
+
+    return weekly_ids
+
+
+def _create_pairs(states, nr_of_weekly_contacts, county_assortativeness, seed):
+    group_codes_per_individual, _ = factorize_assortative_variables(states, ["county"])
+    indexer = create_group_indexer(states, ["county"])
+    fake_params = pd.DataFrame(
+        data=county_assortativeness,
+        columns=["value"],
+        index=pd.MultiIndex.from_tuples(
+            [("assortative_matching", "fake_model", "county")]
+        ),
+    )
+    model_name = "fake_model"
+    first_stage_cum_probs = create_group_transition_probs(
+        states, ["county"], fake_params, model_name
+    )
+    to_match = _create_participation_array(nr_of_weekly_contacts, seed=seed + 1)
+    pair_array = _create_pairs_numba(
+        to_match=to_match,
+        indexer=indexer,
+        first_stage_cum_probs=first_stage_cum_probs,
+        group_codes_per_individual=group_codes_per_individual,
+        seed=seed,
+    )
+    return pair_array
+
+
+@nb.njit
+def _create_pairs_numba(
+    to_match, indexer, first_stage_cum_probs, group_codes_per_individual, seed
+):
+    """
+    Args:
+        to_match (np.ndarry): 2d boolean array with one row per individual
+            and one column sub-contact model.
+        indexer (numba.List): Numba list that maps id of county to a numpy array
+            with the row positions of all individuals from that county.
+        first_stage_cum_probs(numpy.ndarray): Array of shape n_group, n_groups.
+            cum_probs[i, j] is the probability that an individual from group i
+            meets someone from group j or lower.
+        group (np.ndarray): 1d array with assortative matching group ids,
+            coded as integers.
+
+    Returns:
+        pairs_of_workers (np.ndarray): 2d integer array with meeting ids.
+
+    """
+    np.random.seed(seed)
+    unique_group_codes = np.arange(len(first_stage_cum_probs))
+    to_match = to_match.copy()
+    out = np.full(to_match.shape, -1)
+    n_obs, n_models = to_match.shape
+    for m in range(n_models):
+        meeting_id = 0
+        for i in range(n_obs):
+            if to_match[i, m]:
+                group_i = group_codes_per_individual[i]
+                group_j = choose_other_group(
+                    unique_group_codes, first_stage_cum_probs[group_i]
+                )
+                group_j_indices = indexer[group_j]
+                weights = to_match[group_j_indices, m].astype(np.float64)
+                j = choose_other_individual(group_j_indices, weights)
+                if j != -1:
+                    to_match[i, m] = False
+                    to_match[j, m] = False
+                    out[i, m] = meeting_id
+                    out[j, m] = meeting_id
+                    meeting_id += 1
+    return out
+
+
+@nb.njit
+def _create_participation_array(nr_of_contacts, seed):
+    """Draw randomly in which pairs an individual participates.
+
+    Args:
+        nr_of_contacts (pandas.Series): number of contacts, i.e. number of pairs
+            in which every individual is supposed to participate. The specific
+            pair columns will be randomly drawn here.
+        seed (int): seed
+
+    Returns:
+        participation (numpy.ndarray): boolean array of shape
+            (len(nr_of_contacts), nr_of_contacts.max()).
+            If participation[i, mod] is True, individual i was drawn
+            to participate in mod.
+
+    """
+    np.random.seed(seed)
+    n_models = nr_of_contacts.max()
+    n_obs = len(nr_of_contacts)
+    success_prob = nr_of_contacts / n_models
+    participation_array = np.full((n_obs, n_models), False)
+    for i in range(n_obs):
+        prob_i = success_prob[i]
+        for m in range(n_models):
+            participation_array[i, m] = boolean_choice(prob_i)
+
+    return participation_array
--- src/create_initial_states/task_create_background_characteristics.py
+++ src/create_initial_states/task_create_background_characteristics.py
@@ -6,27 +6,129 @@ import pandas as pd
 import pytask
 
 from src.config import BLD
+from src.create_initial_states.add_weekly_ids import add_weekly_ids
 from src.shared import create_age_groups
 from src.shared import create_age_groups_rki
+from src.shared import create_groups_from_dist
 from src.shared import load_dataset
 
 
-@pytask.mark.depends_on(
-    {
-        "hh_data": BLD / "data" / "mossong_2008" / "hh_sample_ger.csv",
-        "hh_probabilities": BLD / "data" / "mossong_2008" / "hh_probabilities.csv",
-        "working_probabilities": BLD
-        / "data"
-        / "population_structure"
-        / "working_shares.pkl",
-        "county_probabilities": BLD / "data" / "counties.parquet",
-    }
-)
-@pytask.mark.produces(BLD / "data" / "inital_states.parquet")
+DEPENDENCIES = {
+    "hh_data": BLD / "data" / "mossong_2008" / "hh_sample_ger.csv",
+    "hh_probabilities": BLD / "data" / "mossong_2008" / "hh_probabilities.csv",
+    "working_probabilities": BLD
+    / "data"
+    / "population_structure"
+    / "working_shares.pkl",
+    "county_probabilities": BLD / "data" / "counties.parquet",
+    "work_daily_dist": BLD
+    / "contact_models"
+    / "empirical_distributions"
+    / "work_recurrent_daily.pkl",
+    "work_weekly_dist": BLD
+    / "contact_models"
+    / "empirical_distributions"
+    / "work_recurrent_weekly.pkl",
+    "other_daily_dist": BLD
+    / "contact_models"
+    / "empirical_distributions"
+    / "other_recurrent_daily.pkl",
+    "other_weekly_dist": BLD
+    / "contact_models"
+    / "empirical_distributions"
+    / "other_recurrent_weekly.pkl",
+}
+
+
+@pytask.mark.depends_on(DEPENDENCIES)
+@pytask.mark.produces(BLD / "data" / "inital_states.pickle")
 def task_create_background_characteristics(depends_on, produces):
     data = {name: load_dataset(path) for name, path in depends_on.items()}
+
+    repeating_contact_distributions = [
+        "work_daily_dist",
+        "work_weekly_dist",
+        "other_daily_dist",
+        "other_weekly_dist",
+    ]
+    group_id_specs = {name: data.pop(name) for name in repeating_contact_distributions}
+
     df = create_background_characteristics(n_households=400_000, seed=3489, **data)
-    df.to_parquet(produces)
+    _check_background_characteristics(df)
+    model_group_ids = create_contact_model_group_ids(
+        df,
+        **group_id_specs,
+        seed=1109,
+    )
+    df = df.merge(model_group_ids, left_index=True, right_index=True, validate="1:1")
+    _check_group_ids(df, **group_id_specs)
+    df.to_pickle(produces)
+
+
+def create_contact_model_group_ids(
+    df, work_daily_dist, work_weekly_dist, other_daily_dist, other_weekly_dist, seed
+):
+    seed = it.count(seed)
+    group_ids = pd.DataFrame(index=df.index)
+
+    # FIRST TEACHER AND STUDENTS
+    # -> new occupation column!
+
+    work_daily_group_sizes = work_daily_dist.copy(deep=True)
+    work_daily_group_sizes.index += 1
+    group_ids["work_daily_group_id"] = create_groups_from_dist(
+        initial_states=df,
+        group_distribution=work_daily_group_sizes,
+        query="occupation == 'working'",
+        assort_bys=["county"],
+        seed=next(seed),
+    )
+
+    other_daily_group_sizes = other_daily_dist.copy(deep=True)
+    other_daily_group_sizes.index += 1
+    group_ids["other_daily_group_id"] = create_groups_from_dist(
+        initial_states=df,
+        group_distribution=other_daily_group_sizes,
+        query=None,
+        assort_bys=["county", "age_group"],
+        seed=next(seed),
+    )
+
+    weekly_work_ids = add_weekly_ids(
+        states=df,
+        weekly_dist=work_weekly_dist,
+        query="occupation =='working'",
+        seed=9958,
+        col_prefix="work_weekly_group_id",
+        county_assortativeness=0.8,
+    )
+
+    group_ids = pd.merge(
+        left=group_ids,
+        right=weekly_work_ids,
+        left_index=True,
+        right_index=True,
+        validate="1:1",
+    )
+
+    weekly_other_ids = add_weekly_ids(
+        states=df,
+        weekly_dist=other_weekly_dist,
+        seed=4748,
+        query=None,
+        col_prefix="other_weekly_group_id",
+        county_assortativeness=0.8,
+    )
+
+    group_ids = pd.merge(
+        left=group_ids,
+        right=weekly_other_ids,
+        left_index=True,
+        right_index=True,
+        validate="1:1",
+    )
+
+    return group_ids
 
 
 def create_background_characteristics(
@@ -334,3 +436,84 @@ def _draw_gender(size, seed=None, p_female=0.5):
     if seed is not None:
         np.random.seed(seed)
     return np.random.choice([True, False], size=size, p=[p_female, 1 - p_female])
+
+
+def _check_background_characteristics(df):
+    """Check that the background characteristics come out right.
+
+    Columns are:
+    - systemically_relevant
+    - age_group
+    - age_group_rki
+    """
+    df = df.copy(deep=True)
+    workers = df.query("occupation == 'working'")
+    df["female"] = df["gender"] == "female"
+    df["male"] = df["gender"] == "male"
+
+    assert df["hh_id"].value_counts().max() <= 8
+    assert df["age"].between(0, 110).all()
+    assert (40 <= df["age"].median()) & (48 >= df["age"].median())
+    assert df["state"].nunique() == 16
+
+    assert df["gender"].value_counts(normalize=True).between(0.48, 0.52).all()
+    young_gender_shares = df.query("age < 18")["gender"].value_counts(normalize=True)
+    assert young_gender_shares.between(0.48, 0.52).all()
+    df.groupby("hh_id")["gender"]
+    old_women_share = (df.query("70 < age < 80")["gender"] == "female").mean()
+    assert old_women_share > 0.53
+    old_women_share = (df.query("age > 80")["gender"] == "female").mean()
+    assert old_women_share > 0.62
+    assert (df.query("occupation == 'working'")["gender"] == "female").mean() < 0.5
+    # given how we assign gender,
+    # men and women should appear in much more than half of households
+    assert df.groupby("hh_id")["female"].any().mean() > 0.75
+    assert df.groupby("hh_id")["male"].any().mean() > 0.75
+
+    occupation_categories = ["working", "school", "stays home", "retired"]
+    assert df["occupation"].isin(occupation_categories).all()
+    assert 0.5 < (df["occupation"] == "working").mean()
+    assert 0.6 > (df["occupation"] == "working").mean()
+    assert 0.08 < (df["occupation"] == "retired").mean()
+    assert 0.12 > (df["occupation"] == "retired").mean()
+    assert 0.12 < (df["occupation"] == "stays home").mean()
+    assert 0.18 > (df["occupation"] == "stays home").mean()
+    assert (df[df["age"].between(6, 14)]["occupation"] == "school").all()
+    assert (df[df["age"] > 70]["occupation"] == "retired").all()
+    assert not df[df["occupation"] != "working"]["systemically_relevant"].any()
+    assert workers["systemically_relevant"].mean() > 0.27
+    assert workers["systemically_relevant"].mean() < 0.35
+
+
+def _check_group_ids(
+    df,
+    work_daily_dist,
+    work_weekly_dist,  # noqa: U100
+    other_daily_dist,
+    other_weekly_dist,  # noqa: U100
+):
+    workers = df.query("occupation == 'working'")
+    non_workers = df.query("occupation != 'working'")
+
+    w_daily_group_vc = workers["work_daily_group_id"].value_counts()
+    # drop -1 category
+    w_daily_group_vc = w_daily_group_vc[w_daily_group_vc > 0]
+    assert w_daily_group_vc.max() <= 16
+    assert (non_workers["work_daily_group_id"] == -1).all()
+    assert (workers["work_daily_group_id"] != -1).all()
+    # compare true and target distribution (incomplete!)
+    w_daily_group_size_shares = w_daily_group_vc.value_counts(normalize=True)
+    assert w_daily_group_size_shares[::-1].is_monotonic
+    goal_w_daily_group_size_shares = work_daily_dist.copy(deep=True)
+    goal_w_daily_group_size_shares.index += 1
+    assert w_daily_group_size_shares.argmax() == goal_w_daily_group_size_shares.argmax()
+
+    o_daily_group_vc = df["other_daily_group_id"].value_counts()
+    # drop -1 category
+    o_daily_group_vc = o_daily_group_vc[o_daily_group_vc > 0]
+    assert o_daily_group_vc.max() <= 6
+    o_daily_group_size_shares = o_daily_group_vc.value_counts(normalize=True)
+    goal_o_daily_group_size_shares = other_daily_dist.copy(deep=True)
+    goal_o_daily_group_size_shares.index += 1
+    diff_btw_o_shares = o_daily_group_size_shares - goal_o_daily_group_size_shares
+    assert np.abs(diff_btw_o_shares).max() < 0.1
