2dc614cda3c11042fe932d32b124d19833b3f17b
Klara Roehrl <klara.roehrl@gmail.com>
2020-12-07 19:13:59 +0100

    Test that the full policies have the right keys.

--- tests/policies/test_full_policy_blocks.py
+++ tests/policies/test_full_policy_blocks.py
@@ -40,27 +40,54 @@ def multipliers():
     return {"work": 0.5, "educ": 0.6, "other": 0.7}
 
 
+@pytest.fixture
+def expected_keys():
+    keys = {
+        "prefix_work",
+        "prefix_educ_school",
+        "prefix_educ_preschool",
+        "prefix_other",
+    }
+    return keys
+
+
 def test_get_only_educ_closed_runs(contact_models, block_info):
-    get_only_educ_closed(contact_models, block_info)
+    res = get_only_educ_closed(contact_models, block_info)
+    assert res.keys() == {"prefix_educ_school", "prefix_educ_preschool"}
 
 
-def test_get_hard_lockdown_runs(contact_models, block_info):
-    get_hard_lockdown(contact_models, block_info, 0.5)
+def test_get_hard_lockdown_runs(contact_models, block_info, expected_keys):
+    res = get_hard_lockdown(contact_models, block_info, 0.5)
+    assert res.keys() == expected_keys
 
 
 def test_get_german_reopening_phase_runs(
-    contact_models, block_info, start_and_end_multipliers
+    contact_models, block_info, start_and_end_multipliers, expected_keys
 ):
-    get_german_reopening_phase(contact_models, block_info, *start_and_end_multipliers)
+    res = get_german_reopening_phase(
+        contact_models, block_info, *start_and_end_multipliers
+    )
+    assert res.keys() == expected_keys
 
 
-def test_get_soft_lockdown_runs(contact_models, block_info, multipliers):
-    get_soft_lockdown(contact_models, block_info, multipliers)
+def test_get_soft_lockdown_runs(contact_models, block_info, multipliers, expected_keys):
+    res = get_soft_lockdown(contact_models, block_info, multipliers)
+    assert res.keys() == expected_keys
 
 
-def test_get_soft_lockdown_with_ab_schooling(contact_models, block_info, multipliers):
-    get_soft_lockdown_with_ab_schooling(contact_models, block_info, multipliers, 12)
+def test_get_soft_lockdown_with_ab_schooling(
+    contact_models, block_info, multipliers, expected_keys
+):
+    res = get_soft_lockdown_with_ab_schooling(
+        contact_models, block_info, multipliers, 12
+    )
+    assert res.keys() == expected_keys
 
 
-def test_get_hard_lockdown_with_ab_schooling(contact_models, block_info, multipliers):
-    get_hard_lockdown_with_ab_schooling(contact_models, block_info, multipliers, 12)
+def test_get_hard_lockdown_with_ab_schooling(
+    contact_models, block_info, multipliers, expected_keys
+):
+    res = get_hard_lockdown_with_ab_schooling(
+        contact_models, block_info, multipliers, 12
+    )
+    assert res.keys() == expected_keys
