c62eda5fd22fb0175cd14ec91adef8b5d489ebb5
Klara Roehrl <klara.roehrl@gmail.com>
2020-12-19 14:02:44 +0100

    Improve plots. Switch sides of plots, remove titles, streamline names.

--- src/simulation/task_plot_christmas_scenarios.py
+++ src/simulation/task_plot_christmas_scenarios.py
@@ -1,3 +1,5 @@
+import itertools
+
 import dask.dataframe as dd
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -5,7 +7,6 @@ import pytask
 import seaborn as sns
 from matplotlib.dates import DateFormatter
 from sid.colors import get_colors
-import itertools
 
 from src.calculate_moments import smoothed_outcome_per_hundred_thousand_rki
 from src.calculate_moments import smoothed_outcome_per_hundred_thousand_sim
@@ -27,71 +28,79 @@ simulation_parametrization = create_christmas_parametrization()
 SIMULATIONS = {entry[:3]: entry[4] for entry in simulation_parametrization}
 
 PRODUCTS = {}
-for mode, scenario in itertools.product(["full", "same_group"], ["optimistic", "pessimistic"]):
-    PRODUCTS[f"{mode}_{scenario}"] = BLD / "simulation" / f"effect_of_private_contact_tracing_{mode}_{scenario}.png"
+for mode, scenario in itertools.product(
+    ["full", "same_group"], ["optimistic", "pessimistic"]
+):
+    PRODUCTS[f"{mode}_{scenario}"] = (
+        BLD / "simulation" / f"effect_of_private_contact_tracing_{mode}_{scenario}.png"
+    )
 
 
 @pytask.mark.depends_on(SIMULATIONS)
 @pytask.mark.produces(PRODUCTS)
 def task_plot_effect_of_private_contact_tracing(depends_on, produces):
-    named_scenarios = {
-        "full": "Weihnachtsfeiern mit wechselnden Haushalten",
-        "same_group": "Weihnachtsfeiern im festen Kreis",
-    }
     for scenario in ["optimistic", "pessimistic"]:
-        for christmas_mode, name in named_scenarios.items():
+        for christmas_mode in ["full", "same_group"]:
             contact_tracing_scenarios = {}
             for (mode, ct_str, sc_str), path in depends_on.items():
                 if mode == christmas_mode and sc_str == scenario:
                     df = dd.read_parquet(path)
                     contact_tracing_scenarios[ct_str] = df
 
-            title = "Die Bedeutung von privater Kontaktnachverfolgung und Selbstquarantäne"
-            fig, axes = plot_scenarios(contact_tracing_scenarios, title=title + "\n" + name)
+            fig, axes = plot_scenarios(contact_tracing_scenarios)
             fig.savefig(
-                produces[f"{christmas_mode}_{scenario}"], dpi=200, bbox_inches="tight", pad_inces=0.5
+                produces[f"{christmas_mode}_{scenario}"],
+                dpi=200,
+                bbox_inches="tight",
+                pad_inches=0.5,
             )
 
 
 PRODUCTS = {}
-for ct_mode, scenario in itertools.product([None, 0.5, 0.1], ["optimistic", "pessimistic"]):
-    PRODUCTS[f"{ct_mode}_{scenario}"] = BLD / "simulation" / f"effect_of_christmas_mode_with_{ct_mode}_contact_tracing_{scenario}.png"
+for ct_mode, scenario in itertools.product(
+    [None, 0.5, 0.1], ["optimistic", "pessimistic"]
+):
+    PRODUCTS[f"{ct_mode}_{scenario}"] = (
+        BLD
+        / "simulation"
+        / f"effect_of_christmas_mode_with_{ct_mode}_contact_tracing_{scenario}.png"
+    )
+
 
 @pytask.mark.depends_on(SIMULATIONS)
 @pytask.mark.produces(PRODUCTS)
 def task_plot_effect_of_christmas_mode(depends_on, produces):
-    named_scenarios = {
-        None: "Ohne private Kontaktnachverfolgung",
-        0.5: "Mit 50 prozentiger privater Kontaktnachverfolgung",
-        0.1: "Mit 90 prozentiger privater Kontaktnachverfolgung",
-    }
     for scenario in ["optimistic", "pessimistic"]:
-        for ct_mode, name in named_scenarios.items():
+        for ct_mode in [None, 0.5, 0.1]:
             christmas_scenarios = {}
             for (mode, ct_str, sc_str), path in depends_on.items():
                 if ct_str == ct_mode and sc_str == scenario:
                     df = dd.read_parquet(path)
                     christmas_scenarios[mode] = df
 
-            title = "Die Bedeutung der Form der Weihnachtstreffen"
-            fig, axes = plot_scenarios(christmas_scenarios, title=title + "\n" + name)
+            fig, axes = plot_scenarios(christmas_scenarios)
             for ax in axes.flatten():
                 ax.grid(axis="y")
-            fig.savefig(produces[f"{ct_mode}_{scenario}"], dpi=200, bbox_inches="tight", pad_inces=0.5)
+            fig.savefig(
+                produces[f"{ct_mode}_{scenario}"],
+                dpi=200,
+                bbox_inches="tight",
+                pad_inches=0.5,
+            )
 
 
-def plot_scenarios(scenarios, title):
+def plot_scenarios(scenarios):
     name_to_label = {
         None: "Ohne private\nKontaktnachverfolgung",
         0.5: "Mit 50 prozentiger privater\nKontaktnachverfolgung",
         0.1: "Mit 90 prozentiger privater\nKontaktnachverfolgung",
-        "full": "Weihnachtsfeiern mit\nwechselnden Haushalten",
-        "same_group": "Weihnachtsfeiern\nim festen Kreis",
+        "full": "Weihnachtsfeiern mit\nwechselnden Personenkreisen",
+        "same_group": "Weihnachtsfeiern mit\neinem festen Personenkreis",
     }
 
     outcome_vars = [
-        ("new_known_case", "Beobachtete Inzidenz"),
         ("newly_infected", "Tatsächliche Inzidenz"),
+        ("new_known_case", "Beobachtete Inzidenz"),
     ]
     fig, axes = plt.subplots(ncols=len(outcome_vars), figsize=(8, 4), sharex=True)
 
@@ -144,10 +153,10 @@ def plot_scenarios(scenarios, title):
         ax.set_ylim(bottom=50, top=top)
 
     fig.autofmt_xdate()
-    fig.suptitle(title, fontsize=14)
 
     fig.tight_layout()
-    axes[0].legend(loc="upper center", bbox_to_anchor=(0.5, -0.5, 1, 0.2), ncol=3)
+    axes[1].legend(loc="upper center", bbox_to_anchor=(-0.8, -0.5, 1, 0.2), ncol=3)
+    axes[0].get_legend().remove()
     return fig, axes
 
 
@@ -172,7 +181,7 @@ def plot_outcome(
         window=window,
         min_periods=min_periods,
         take_logs=False,
-        center=outcome == "newly_infected",
+        center=False,
     )
 
     if isinstance(daily_incidence, dd.Series):
@@ -181,15 +190,11 @@ def plot_outcome(
     data = weekly_incidence.reset_index()
     sns.lineplot(data=data, x="date", y=outcome, ax=ax, label=label, color=color)
 
-
     ax.set_ylabel("Geglättete wöchentliche \nNeuinfektionen pro 100 000")
     ax.set_xlabel("Datum")
 
     ax.grid(axis="y")
     ax.set_axisbelow(True)
-
-    if outcome == "newly_infected":
-        ax.get_legend().remove()
 
     date_form = DateFormatter("%d.%m")
     ax.xaxis.set_major_formatter(date_form)
