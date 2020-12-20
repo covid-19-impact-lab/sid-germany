5bc5dd0111022ca368223d53e988b75c8e0a4cfa
Klara Roehrl <klara.roehrl@gmail.com>
2020-12-20 15:08:11 +0100

    Add figure with four different simulations. Fix legend for that plot.

--- src/simulation/task_plot_christmas_scenarios.py
+++ src/simulation/task_plot_christmas_scenarios.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pytask
 import seaborn as sns
+from matplotlib.dates import AutoDateLocator
 from matplotlib.dates import DateFormatter
 from sid.colors import get_colors
 
@@ -35,6 +36,16 @@ for mode, optimism in itertools.product(
         BLD / "simulation" / f"effect_of_private_contact_tracing_{mode}_{optimism}.png"
     )
 
+NAME_TO_LABEL = {
+    None: "Ohne private\nKontaktnachverfolgung",
+    0.5: "Mit 50 prozentiger privater\nKontaktnachverfolgung",
+    0.1: "Mit 90 prozentiger privater\nKontaktnachverfolgung",
+    "full": "Weihnachtsfeiern mit\nwechselnden Personenkreisen",
+    "same_group": "Weihnachtsfeiern mit\neinem festen Personenkreis",
+    "optimistic": "Optimistisch",
+    "pessimistic": "Pessimistisch",
+}
+
 
 @pytask.mark.depends_on(SIMULATIONS)
 @pytask.mark.produces(PRODUCTS)
@@ -45,7 +56,8 @@ def task_plot_effect_of_private_contact_tracing(depends_on, produces):
             for (mode, ct_str, optim_str), path in depends_on.items():
                 if mode == christmas_mode and optim_str == optimism:
                     df = dd.read_parquet(path)
-                    contact_tracing_scenarios[ct_str] = df
+                    label = NAME_TO_LABEL[ct_str]
+                    contact_tracing_scenarios[label] = df
 
             fig, axes = plot_scenarios(contact_tracing_scenarios)
             fig.savefig(
@@ -76,7 +88,7 @@ def task_plot_effect_of_christmas_mode(depends_on, produces):
             for (mode, ct_str, optimism_str), path in depends_on.items():
                 if ct_str == ct_mode and optimism_str == optimism:
                     df = dd.read_parquet(path)
-                    christmas_scenarios[mode] = df
+                    christmas_scenarios[NAME_TO_LABEL[mode]] = df
 
             fig, axes = plot_scenarios(christmas_scenarios)
             for ax in axes.flatten():
@@ -108,7 +120,7 @@ def task_plot_effect_of_optimism(depends_on, produces):
             for (mode, ct_str, optimism_str), path in depends_on.items():
                 if ct_str == ct_mode and christmas_mode == mode:
                     df = dd.read_parquet(path)
-                    scenarios[optimism_str] = df
+                    scenarios[NAME_TO_LABEL[optimism_str]] = df
 
             fig, axes = plot_scenarios(scenarios)
             for ax in axes.flatten():
@@ -121,16 +133,60 @@ def task_plot_effect_of_optimism(depends_on, produces):
             )
 
 
-def plot_scenarios(scenarios):
-    name_to_label = {
-        None: "Ohne private\nKontaktnachverfolgung",
-        0.5: "Mit 50 prozentiger privater\nKontaktnachverfolgung",
-        0.1: "Mit 90 prozentiger privater\nKontaktnachverfolgung",
+@pytask.mark.depends_on(SIMULATIONS)
+@pytask.mark.produces(BLD / "simulation" / "figure2.png")
+def task_plot_figure_two(depends_on, produces):
+    # christmas_mode, contact_tracing, optimism
+    label_dict = {
         "full": "Weihnachtsfeiern mit\nwechselnden Personenkreisen",
         "same_group": "Weihnachtsfeiern mit\neinem festen Personenkreis",
-        "optimistic": "Optimistisch",
-        "pessimistic": "Pessimistisch",
+        "optimistic": "optimistisches Szenario",
+        "pessimistic": "pessimistisches Szenario",
     }
+    scenario_tuples = [
+        ("full", "optimistic"),
+        ("same_group", "optimistic"),
+        ("full", "pessimistic"),
+        ("same_group", "pessimistic"),
+    ]
+    scenarios = {}
+    for (christmas_mode, optimism) in scenario_tuples:
+        label = f"{label_dict[christmas_mode]},\n{label_dict[optimism]}"
+        scenarios[label] = dd.read_parquet(depends_on[(christmas_mode, None, optimism)])
+
+    colors = get_colors("ordered", 3)[:2] * 2  # color identifies christmas mode
+    linestyles = ["-", "-", "--", "--"]  # line style gives optimism
+
+    fig, axes = plot_scenarios(
+        scenarios,
+        colors=colors,
+        linestyles=linestyles,
+    )
+    for ax in axes.flatten():
+        ax.grid(axis="y")
+
+    # drop shaded area labels
+    handles, labels = axes[1].get_legend_handles_labels()
+    handles = handles[:-2]
+    labels = labels[:-2]
+    axes[1].legend(
+        handles, labels, loc="upper center", bbox_to_anchor=(-0.8, -0.5, 1, 0.2), ncol=3
+    )
+
+    fig.savefig(
+        produces,
+        dpi=200,
+        bbox_inches="tight",
+        pad_inches=0.5,
+    )
+
+
+DEFAULT_COLORS = get_colors("ordered", 3)
+
+
+def plot_scenarios(scenarios, colors=DEFAULT_COLORS, linestyles=None):
+    if linestyles is None:
+        linestyles = ["-"] * len(scenarios)
 
     outcome_vars = [
         ("newly_infected", "Tatsächliche Inzidenz"),
@@ -140,15 +196,15 @@ def plot_scenarios(scenarios):
 
     for ax, (outcome, ax_title) in zip(axes, outcome_vars):
         ax.set_title(ax_title)
-        colors = get_colors("ordered", 3)
-        for color, (name, df) in zip(colors, scenarios.items()):
+        for color, ls, (label, df) in zip(colors, linestyles, scenarios.items()):
             plot_outcome(
                 df=df,
                 outcome=outcome,
                 ax=ax,
-                label=name_to_label[name],
+                label=label,
                 color=color,
                 window=7,
+                linestyle=ls,
             )
 
         ax.fill_between(
@@ -164,9 +220,17 @@ def plot_scenarios(scenarios):
             y1=0,
             y2=2000,
             alpha=0.2,
-            label="Harter Lockdown\nvor Weihnachten",
+            label="Harter Lockdown",
             color=get_colors("ordered", 3)[0],
         )
+        ax.fill_between(
+            x=[pd.Timestamp("2020-12-26"), pd.Timestamp("2021-01-10")],
+            y1=0,
+            y2=2000,
+            alpha=0.2,
+            color=get_colors("ordered", 3)[0],
+        )
+
         if outcome == "new_known_case":
             rki_data = pd.read_pickle(
                 BLD / "data" / "processed_time_series" / "rki.pkl"
@@ -185,12 +249,16 @@ def plot_scenarios(scenarios):
             )
         top = 350 if outcome == "new_known_case" else 1400
         ax.set_ylim(bottom=50, top=top)
+        ax.set_xlim(pd.Timestamp("2020-12-01"), pd.Timestamp("2021-01-10"))
 
     fig.autofmt_xdate()
 
     fig.tight_layout()
     axes[1].legend(loc="upper center", bbox_to_anchor=(-0.8, -0.5, 1, 0.2), ncol=3)
     axes[0].get_legend().remove()
+    loc = AutoDateLocator(minticks=5, maxticks=8)
+    for ax in axes:
+        ax.xaxis.set_major_locator(loc)
     return fig, axes
 
 
@@ -200,6 +268,7 @@ def plot_outcome(
     outcome,
     label,
     color,
+    linestyle="-",
     window=7,
     min_periods=1,
 ):
@@ -222,7 +291,15 @@ def plot_outcome(
         daily_incidence = daily_incidence.compute()
     weekly_incidence = 7 * daily_incidence
     data = weekly_incidence.reset_index()
-    sns.lineplot(data=data, x="date", y=outcome, ax=ax, label=label, color=color)
+    sns.lineplot(
+        data=data,
+        x="date",
+        y=outcome,
+        ax=ax,
+        label=label,
+        color=color,
+        linestyle=linestyle,
+    )
 
     ax.set_ylabel("Geglättete wöchentliche \nNeuinfektionen pro 100 000")
     ax.set_xlabel("Datum")
