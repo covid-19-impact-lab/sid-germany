3e505860116ce5a6b6022f0a603fa44f91bb5b95
Klara Roehrl <klara.roehrl@gmail.com>
2021-02-10 12:33:21 +0100

    Add and use old sid functions to get simulations to run through.

--- src/create_initial_states/add_weekly_ids.py
+++ src/create_initial_states/add_weekly_ids.py
@@ -1,15 +1,20 @@
 import itertools as it
+from typing import Dict
+from typing import List
 
 import numba as nb
 import numpy as np
 import pandas as pd
+from numba.typed import List as NumbaList
+from sid.config import DTYPE_INDEX
 from sid.contacts import boolean_choice
 from sid.contacts import choose_other_group
 from sid.contacts import choose_other_individual
-from sid.contacts import create_group_indexer
-from sid.matching_probabilities import create_group_transition_probs
 from sid.shared import factorize_assortative_variables
 
+from src.create_initial_states.create_group_transition_probs import (
+    create_group_transition_probs,
+)
 from src.shared import draw_from_distribution_for_subset
 
 
@@ -77,7 +82,7 @@ def _create_pairs(states, nr_of_weekly_contacts, county_assortativeness, seed):
     group_codes_per_individual, _ = factorize_assortative_variables(
         states, ["county"], is_recurrent=False
     )
-    indexer = create_group_indexer(states, ["county"], is_recurrent=False)
+    indexer = _create_group_indexer(states, ["county"], is_recurrent=False)
     fake_params = pd.DataFrame(
         data=county_assortativeness,
         columns=["value"],
@@ -87,7 +92,7 @@ def _create_pairs(states, nr_of_weekly_contacts, county_assortativeness, seed):
     )
     model_name = "fake_model"
     first_stage_cum_probs = create_group_transition_probs(
-        states, ["county"], fake_params, model_name
+        states=states, assort_by=["county"], params=fake_params, model_name=model_name
     )
     to_match = _create_participation_array(nr_of_weekly_contacts, seed=seed + 1)
     pair_array = _create_pairs_numba(
@@ -173,3 +178,51 @@ def _create_participation_array(nr_of_contacts, seed):
             participation_array[i, m] = boolean_choice(prob_i)
 
     return participation_array
+
+
+def _create_group_indexer(
+    states: pd.DataFrame, assort_by: Dict[str, List[str]], is_recurrent
+) -> nb.typed.List:
+    """Create the group indexer.
+
+    The indexer is a list where the positions correspond to the group number defined by
+    assortative variables. The values inside the list are one-dimensional integer arrays
+    containing the indices of states belonging to the group.
+
+    If there are no assortative variables, all individuals are assigned to a single
+    group with code 0 and the indexer is a list where the first position contains all
+    indices of states.
+
+    For efficiency reasons, we assign each group a number instead of identifying by
+    the values of the assort_by variables directly.
+
+    Note: This function is from sid commit 206886a14eeb3257deb71db91aba4e7fb2385fc2.
+
+    Args:
+        states (pandas.DataFrame): See :ref:`states`
+        assort_by (List[str]): List of variables that influence matching probabilities.
+        is_recurrent (bool)
+
+    Returns:
+        indexer (numba.typed.List): The i_th entry are the indices of the i_th group.
+
+    """
+    states = states.reset_index()
+    if assort_by:
+        groups = states.groupby(assort_by).groups
+        _, group_codes_values = factorize_assortative_variables(
+            states, assort_by, is_recurrent
+        )
+
+        indexer = NumbaList()
+        for group in group_codes_values:
+            # the keys of groups are not tuples if there was just one assort_by variable
+            # but the group_codes_values are.
+            group = group[0] if isinstance(group, tuple) and len(group) == 1 else group
+            indexer.append(groups[group].to_numpy(dtype=DTYPE_INDEX))
+
+    else:
+        indexer = NumbaList()
+        indexer.append(states.index.to_numpy(DTYPE_INDEX))
+
+    return indexer
