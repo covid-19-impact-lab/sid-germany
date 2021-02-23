afb85be4098816c00637ab3532545e2824ee8cb9
Klara Roehrl <klara.roehrl@gmail.com>
2021-02-23 11:46:20 +0100

    Add visualizations of test shares by age group and positivity rates by age group.

--- src/testing/task_prepare_ars_data.py
+++ src/testing/task_prepare_ars_data.py
@@ -1,36 +1,64 @@
 """Prepare the ARS data."""
+import matplotlib.pyplot as plt
 import pandas as pd
 import pytask
+import seaborn as sns
+from sid.colors import get_colors
 
 from src.config import BLD
 from src.config import SRC
+from src.simulation.plotting import style_plot
 from src.testing.task_calculate_test_capacity import (
     get_date_from_year_and_week,
 )
 
+plt.rcParams.update(
+    {
+        "axes.spines.right": False,
+        "axes.spines.top": False,
+        "legend.frameon": False,
+    }
+)
+
+sns.set_palette(get_colors("categorical", 12))
+
+
 OUT_PATH = BLD / "data" / "testing"
 
 PRODUCTS = {
-    "full_weekly": OUT_PATH / "ars_cleaned_full_weekly.csv",
     "test_shares_by_age_group": OUT_PATH / "test_shares_by_age_group.csv",
     "positivity_rate_by_age_group": OUT_PATH / "positivity_rate_by_age_group.csv",
+    "test_shares_by_age_group_png": OUT_PATH / "test_shares_by_age_group.png",
+    "positivity_rate_by_age_group_png": OUT_PATH / "positivity_rate_by_age_group.png",
 }
 
 
 @pytask.mark.depends_on(SRC / "original_data" / "testing" / "ars_data_raw.xlsx")
 @pytask.mark.produces(PRODUCTS)
 def task_prepare_ars_data(depends_on, produces):
+    """Calculate and save quantaties of interest of the ARS data.
+
+    We do not export the ARS data at the moment because:
+    1. it is weekly and not daily frequency data (not relevant for shares and rates)
+    2. it would be necessary to upscale the number of tests to account for
+       the fact that only a fraction of laboratories report ARS data.
+
+    """
     ars = pd.read_excel(depends_on)
     ars = _clean_ars_data(ars)
 
     test_shares_by_age_group = _calculate_test_shares_by_age_group(ars)
     test_shares_by_age_group.to_csv(produces["test_shares_by_age_group"])
 
+    fig, ax = _plot_frame(test_shares_by_age_group)
+    fig.savefig(produces["test_shares_by_age_group_png"])
+
     positivity_rates = ars["positivity_rate_by_age_group"].unstack()
     positivity_rates = _convert_from_weekly_to_daily(positivity_rates)
     positivity_rates.to_csv(produces["positivity_rate_by_age_group"])
 
-    ars.to_csv(produces["full_weekly"])
+    fig, ax = _plot_frame(positivity_rates)
+    fig.savefig(produces["positivity_rate_by_age_group_png"])
 
 
 def _clean_ars_data(ars):
@@ -52,12 +80,14 @@ def _clean_ars_data(ars):
     cleaned["n_positive_tests"] = (
         cleaned["n_tests"] * cleaned["positivity_rate_by_age_group"]
     )
-    cleaned["age_group"] = pd.Categorical(
+    cleaned["age_group"] = cleaned["age_group"].replace({">=80": "80-100"})
+    cleaned["age_group_rki"] = pd.Categorical(
         cleaned["age_group"],
         ordered=True,
-        categories=["0-4", "5-14", "15-34", "35-59", "60-79", ">=80"],
+        categories=["0-4", "5-14", "15-34", "35-59", "60-79", "80-100"],
     )
-    cleaned = cleaned.set_index(["date", "age_group"])
+    cleaned = cleaned.drop(columns=["age_group"])
+    cleaned = cleaned.set_index(["date", "age_group_rki"])
     return cleaned
 
 
@@ -106,3 +136,11 @@ def _convert_from_weekly_to_daily(short):
     expanded = short.reindex(new_dates)
     expanded = expanded.fillna(method="backfill")
     return expanded
+
+
+def _plot_frame(df):
+    fig, ax = plt.subplots(figsize=(15, 5))
+    for col in df:
+        sns.lineplot(ax=ax, x=df.index, y=df[col], label=col)
+    fig, ax = style_plot(fig, ax)
+    return fig, ax
