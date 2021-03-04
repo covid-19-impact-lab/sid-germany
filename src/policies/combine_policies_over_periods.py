5016ef743f40be94c162cc3f58e8bcb3fdb56d49
Klara Roehrl <klara.roehrl@gmail.com>
2021-03-04 13:56:56 +0100

    Update policies up to today, including emergency care, graduating classes and primary schools.

--- src/policies/combine_policies_over_periods.py
+++ src/policies/combine_policies_over_periods.py
@@ -5,6 +5,136 @@ from src.policies.full_policy_blocks import get_lockdown_with_multipliers
 from src.policies.policy_tools import combine_dictionaries
 
 
+def _get_jan_first_half_educ_options(hygiene_multiplier=0.8):
+    """Get the a_b_educ_options and emergency_options for the 1st half of January.
+
+    1 in 10 children in 5th and 6th grade attends in emergency care (source only
+    for Bavaria, mid January: https://bit.ly/3sGHZbJ)
+
+    Jump from ~25% to 33% between first Jan week and later (https://bit.ly/3uGL1Pb).
+    This could be a vacation effect with more parents returning to work.
+
+    This abstracts from graduating classes opening early in some states, such as
+    Berlin (11 of Jan, https://bit.ly/385iCZk).
+
+    """
+    emergency_options = {
+        "school": {
+            "hygiene_multiplier": hygiene_multiplier,
+            # emergency care only until 6th grade
+            "always_attend_query": "educ_contact_priority > 0.9",
+        },
+        "preschool": {
+            "hygiene_multiplier": hygiene_multiplier,
+            "always_attend_query": "educ_contact_priority > 0.75",
+        },
+        "nursery": {
+            "hygiene_multiplier": hygiene_multiplier,
+            "always_attend_query": "educ_contact_priority > 0.75",
+        },
+    }
+    educ_options = {"a_b_educ_options": None, "emergency_options": emergency_options}
+    return educ_options
+
+
+def _get_mid_jan_to_mid_feb_educ_options(
+    young_children_multiplier=0.8, school_multiplier=0.5
+):
+    """Get the a_b_educ_options and emergency_options for the 2nd half of January.
+
+    In the secnd half of January, approx. 1 / 3 of children were in emergency care
+    (https://bit.ly/3uGL1Pb).
+
+    sources:
+        - Berlin: <40% (https://bit.ly/304R5ml)
+        - Niedersachsen: 38% (https://bit.ly/2PtPdSb)
+
+    In addition, many states opened graduating classes. We open them at the federal
+    level in A / B schooling on Jan 16th. This abstracts from state variety:
+        - Bavaria: started on 1 Feb (https://bit.ly/3e4p1YE)
+        - Baden-Württemberg started on 18 Jan (https://bit.ly/2Ofq9O7)
+        - Berlin started on  11 Jan (https://bit.ly/385iCZk)
+
+    Sources:
+        - https://taz.de/Schulen-in-Coronazeiten/!5753515/
+        - https://tinyurl.com/2jfm4tp8
+
+    """
+    emergency_options = {
+        "preschool": {
+            "hygiene_multiplier": young_children_multiplier,
+            "always_attend_query": "educ_contact_priority > 0.67",
+        },
+        "nursery": {
+            "hygiene_multiplier": young_children_multiplier,
+            "always_attend_query": "educ_contact_priority > 0.67",
+        },
+    }
+    a_b_educ_options = {
+        "school": {
+            "others_attend": False,
+            "hygiene_multiplier": school_multiplier,
+            # to cover graduating classes
+            "subgroup_query": "age in [16, 17, 18]",
+            # Demand seems to be lower the older the children
+            # but only data from Bavaria available: https://bit.ly/3sGHZbJ
+            "always_attend_query": "educ_contact_priority > 0.9",
+            # only very anecdotally the current most common rhythm.
+            "rhythm": "daily",
+        }
+    }
+    educ_options = {
+        "a_b_educ_options": a_b_educ_options,
+        "emergency_options": emergency_options,
+    }
+    return educ_options
+
+
+def _get_educ_options_starting_feb_22(school_multiplier=0.5):
+    """Get the a_b_educ_options and emergency_options from 22 Feb onwards.
+
+    This assumes that nurseries and preschools are open normally (i.e. only the
+    general educ_multiplier is applied to them). Schools open for primary students
+    and graduating classes in A/B while maintaining emergency care for children
+    with very high educ_contact_priority (>0.9).
+
+    Summary of actual policies of states with >=8 mio inhabitants:
+        BW: nurseries and primary schools open Feb 22nd. No mention of preschools.
+            graduating classes attend.
+        BY: nurseries and primary schools open Feb 22nd. No mention of preschools.
+            graduating classes attend.
+        Niedersachsen: primaries and graduating classes in A/B since January.
+            nurseries only in emergency care. No mention of preschools.
+        NRW: nurseries pretty normal since Feb 22nd. Primaries and graduating classes
+            start Feb 22nd, A/B or full depending on local incidecne.
+            (https://bit.ly/3uSp6Ey)
+
+    Sources:
+        - https://www.mdr.de/brisant/corona-schule-geoeffnet-100.html
+        - https://bit.ly/2O3aS3h
+
+    """
+    emergency_options = {}
+    a_b_educ_options = {
+        "school": {
+            "others_attend": False,
+            "hygiene_multiplier": school_multiplier,
+            # to cover graduating classes
+            "subgroup_query": "age in [16, 17, 18]",
+            # Demand seems to be lower the older the children
+            # but only data from Bavaria available: https://bit.ly/3sGHZbJ
+            "always_attend_query": "educ_contact_priority > 0.9",
+            # only very anecdotally the current most common rhythm.
+            "rhythm": "daily",
+        }
+    }
+    educ_options = {
+        "a_b_educ_options": a_b_educ_options,
+        "emergency_options": emergency_options,
+    }
+    return educ_options
+
+
 def get_enacted_policies_of_2021(
     contact_models,
     scenario_start,
@@ -34,37 +164,30 @@ def get_enacted_policies_of_2021(
             contact_models=contact_models,
             block_info={
                 "start_date": "2021-01-02",
-                "end_date": "2021-01-31",
-                "prefix": "january",
+                "end_date": "2021-01-15",
+                "prefix": "january1",
             },
             multipliers={
-                "educ": 0.0,
+                "educ": None,
                 "work": work_multiplier,
                 "other": other_multiplier,
             },
+            **_get_jan_first_half_educ_options(),
         ),
         get_lockdown_with_multipliers(
             contact_models=contact_models,
             block_info={
-                "start_date": "2021-02-01",
+                "start_date": "2021-01-16",
                 "end_date": "2021-02-21",
-                "prefix": "february1",
+                "prefix": "january2",
             },
             multipliers={
-                "educ": 0.0,
+                "educ": None,
                 "work": work_multiplier,
                 "other": other_multiplier,
             },
+            **_get_mid_jan_to_mid_feb_educ_options(),
         ),
-        # very varied schooling across German states
-        # very often A / B schooling, only primaries (1-4)
-        # and graduating classes are open
-        # we simplify to have up to grade 5 open everywhere
-        # in A / B schooling and older youths stay home.
-        # We ignore Notbetreuung like this.
-        # sources:
-        # - https://taz.de/Schulen-in-Coronazeiten/!5753515/
-        # - https://tinyurl.com/2jfm4tp8
         get_lockdown_with_multipliers(
             contact_models=contact_models,
             block_info={
@@ -75,17 +198,12 @@ def get_enacted_policies_of_2021(
             multipliers={
                 # in fall the educ multiplier was estimated to be 0.8.
                 # Here, it is 0.5 to capture additional hygiene measures.
+                # This multiplier applies to nurseries and preschools.
                 "educ": 0.5,
                 "work": work_multiplier,
                 "other": other_multiplier,
             },
-            a_b_educ_options={
-                "school": {
-                    "subgroup_query": "age <= 12 | age >15",
-                    "others_attend": False,
-                    "hygiene_multiplier": 0.5,
-                },
-            },
+            **_get_educ_options_starting_feb_22(),
         ),
     ]
     return combine_dictionaries(to_combine)
