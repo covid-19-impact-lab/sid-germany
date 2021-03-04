3781101fcc392e665db6dee5a27f95d2d413fc6c
Klara Roehrl <klara.roehrl@gmail.com>
2021-03-04 16:59:30 +0100

    Add educ_contact_priority to initial states.

--- src/create_initial_states/task_create_background_characteristics.py
+++ src/create_initial_states/task_create_background_characteristics.py
@@ -109,6 +109,12 @@ def _build_initial_states(
         seed=555,
     )
 
+    adult_at_home = (df["occupation"].isin(["stays home", "retired"])) & (
+        df["age"] >= 18
+    )
+    df["adult_in_hh_at_home"] = adult_at_home.groupby(df["hh_id"]).transform(any)
+    df["educ_contact_priority"] = _create_educ_contact_priority(df)
+
     df.index.name = "index"
     df = _only_keep_relevant_columns(df)
     df = df.sample(frac=1).reset_index(drop=True)
@@ -251,6 +257,32 @@ def _create_occupation(df):
     return occupation
 
 
+def _create_educ_contact_priority(df):
+    """Create an educ contact priority Series.
+
+    The values are 0 for anyone above 12 and uniform from 0 to 1 below.
+    Children in households where no adult is retired or stays home have a
+    higher educ contact priority than chidldren where at least one adult is
+    at home.
+
+    """
+    entitled_to_emergency_care = df.eval("age < 13 & ~adult_in_hh_at_home")
+    share_entitled_children = entitled_to_emergency_care[df["age"] < 13].mean()
+    threshold = 1 - share_entitled_children
+
+    educ_contact_priority = pd.Series(0, index=df.index)
+    low_priorities = np.random.uniform(low=0, high=threshold, size=len(df))
+    high_priorities = np.random.uniform(low=threshold, high=1, size=len(df))
+    not_low_priority_children = entitled_to_emergency_care | (df["age"] >= 13)
+    educ_contact_priority = educ_contact_priority.where(
+        ~entitled_to_emergency_care, high_priorities
+    )
+    educ_contact_priority = educ_contact_priority.where(
+        not_low_priority_children, low_priorities
+    )
+    return educ_contact_priority
+
+
 def _only_keep_relevant_columns(df):
     keep = [
         "age",  # used by `implement_a_b_school_system_above_age`
@@ -282,6 +314,8 @@ def _only_keep_relevant_columns(df):
         "school_group_id_0_a_b",
         "school_group_id_1_a_b",
         "school_group_id_2_a_b",
+        "adult_in_hh_at_home",
+        "educ_contact_priority",
         "work_daily_group_id",
         "work_weekly_group_id_0",
         "work_weekly_group_id_1",
