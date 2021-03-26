932d125ea785b447c631a5fcd448778b720dd1a0
Klara Roehrl <klara.roehrl@gmail.com>
2021-03-26 17:35:19 +0100

    Make docstring comply with flake8.

--- src/config.py
+++ src/config.py
@@ -7,9 +7,9 @@ FAST_FLAG = "debug"
 If 'debug' only the debug initial states are used and only one run of every scenario
 is done. Do **not** interpret the results.
 
-If 'verify' there are only 10 seeds and only the base scenario done in the fall scenarios.
-In the main_predictions we use 5 seeds for each scenario. This means there 30 simulation
-runs overall.
+If 'verify' only 10 seeds and the base scenario are done in the fall scenarios.
+In the main_predictions we use 5 seeds for each scenario.
+This means there 30 simulation runs overall.
 
 If 'full' 20 seeds are used for each scenario.
 
