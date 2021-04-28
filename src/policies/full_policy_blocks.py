b66b8ff8197fe48ca4fc05044b489da95db2ebb3
Klara Röhrl <klara.roehrl@gmail.com>
2021-04-28 15:19:44 +0200

    Remove redundant functions and their tests. (#62)

--- src/policies/full_policy_blocks.py
+++ src/policies/full_policy_blocks.py
@@ -23,69 +23,8 @@ Thus we do not use anything but multipliers here!
 from src.policies.domain_level_policy_blocks import implement_general_schooling_policy
 from src.policies.domain_level_policy_blocks import reduce_other_models
 from src.policies.domain_level_policy_blocks import reduce_work_models
-from src.policies.domain_level_policy_blocks import reopen_educ_models
-from src.policies.domain_level_policy_blocks import reopen_other_models
-from src.policies.domain_level_policy_blocks import reopen_work_models
 from src.policies.domain_level_policy_blocks import shut_down_other_models
 from src.policies.policy_tools import combine_dictionaries
-
-
-def get_german_reopening_phase(
-    contact_models,
-    block_info,
-    start_multipliers,
-    end_multipliers,
-    educ_switching_date="2020-08-01",
-    educ_reopening_dates=None,
-):
-    """Model German summer 2020 as gradual reopening of schools, economy and leisure.
-
-    This function is very specific and not meant to be used in any other context than
-    to replicate actual German policies from April to September during estimation.
-
-    Args:
-        start_multipliers (dict): Dictionary with the following entries:
-
-            - "educ": Multiplier for activity in educ models after reopening but before
-              summer vacation.
-            - "work": Multiplier for work contacts at beginning of reopening phase. Note
-              that essential workers always work. Thus the multiplier is the share of
-              active non-essential workers.
-            - "other": Multiplier for non-work and non-educ contacts at beginning of
-              reopening phase.
-        end_multipliers (dict): Dictionary with same entries as start_multipliers but
-            for activity levels at the end of the reopening phase.
-        educ_switching_date (str or pandas.Timestamp): Date at which the multiplier
-            switches from start to end multiplier in education models.
-        educ_reopening_dates (dict): Maps German federal states to reopening dates
-            for education models.
-
-    """
-    to_combine = [
-        reopen_educ_models(
-            contact_models=contact_models,
-            block_info=block_info,
-            start_multiplier=start_multipliers["educ"],
-            end_multiplier=end_multipliers["educ"],
-            switching_date=educ_switching_date,
-            reopening_dates=educ_reopening_dates,
-        ),
-        reopen_work_models(
-            contact_models=contact_models,
-            block_info=block_info,
-            start_multiplier=start_multipliers["work"],
-            end_multiplier=end_multipliers["work"],
-        ),
-        reopen_other_models(
-            contact_models=contact_models,
-            block_info=block_info,
-            start_multiplier=start_multipliers["other"],
-            end_multiplier=end_multipliers["other"],
-        ),
-    ]
-
-    policies = combine_dictionaries(to_combine)
-    return policies
 
 
 def get_lockdown_with_multipliers(
