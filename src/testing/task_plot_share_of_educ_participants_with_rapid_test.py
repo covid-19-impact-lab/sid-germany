e9ca181febac7e344419d75c2b2a49dd3d1e0d6e
Klara Roehrl <klara.roehrl@gmail.com>
2021-04-30 09:50:17 +0200

    Plot share of educ participants getting a rapid test.

---
+++ src/testing/task_plot_share_of_educ_participants_with_rapid_test.py
@@ -0,0 +1,50 @@
+import warnings
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import pytask
+import seaborn as sns
+
+from src.config import BLD
+from src.simulation.plotting import style_plot
+from src.testing.shared import get_piecewise_linear_interpolation
+
+
+@pytask.mark.depends_on(BLD / "params.pkl")
+@pytask.mark.produces(
+    BLD / "data" / "testing" / "share_of_educ_participants_with_rapid_test.png"
+)
+def task_plot_share_of_educ_participants_with_rapid_test(depends_on, produces):
+    params = pd.read_pickle(depends_on)
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            "ignore", message="indexing past lexsort depth may impact performance."
+        )
+        educ_workers_params = params.loc[("rapid_test_demand", "educ_worker_shares")]
+        students_params = params.loc[("rapid_test_demand", "student_shares")]
+
+    share_educ_workers = get_piecewise_linear_interpolation(educ_workers_params)
+    share_educ_workers = share_educ_workers.loc["2021-01-01":]
+
+    share_students = get_piecewise_linear_interpolation(students_params)
+    share_students = share_students.loc["2021-01-01":]
+
+    fig, ax = plt.subplots(figsize=(8, 3))
+    sns.lineplot(
+        x=share_educ_workers.index,
+        y=share_educ_workers,
+        ax=ax,
+        label="Teachers (School, Preschool, Nursery)",
+    )
+    sns.lineplot(
+        x=share_students.index,
+        y=share_students,
+        ax=ax,
+        label="School Students",
+    )
+    ax.set_title("Share Educ Participants Getting Rapid Test")
+    fig, ax = style_plot(fig, ax)
+    fig.tight_layout()
+
+    fig.savefig(produces)
+    plt.close()
