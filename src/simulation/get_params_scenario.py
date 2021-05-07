4571eb924c7636ed277e0d4715901dced21dc573
Klara Roehrl <klara.roehrl@gmail.com>
2021-05-07 18:12:47 +0200

    New implementation of the simulation.

---
+++ src/simulation/get_params_scenario.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+from src.config import BLD
+
+
+def get_params_scenario(scenario):
+    """Return params fitting to the given scenario.
+
+    Note that many scenarios must be implemented through `load_simulation_inputs`.
+
+    Args:
+        scenario (str): One of ["baseline"].
+
+    Returns:
+        params (pandas.DataFrame): params adjusted to the scenario.
+
+    """
+    params = pd.read_pickle(BLD / "params.pkl")
+
+    if scenario == "baseline":
+        pass
+    else:
+        raise ValueError(
+            "The params scenario must be one of ['baseline']. "
+            f"You specified {scenario}."
+        )
+    return params
