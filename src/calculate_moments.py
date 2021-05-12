04d330883cebf14159060c27d72bd59abcd3f68c
Janos Gabler <janos.gabler@gmail.com>
2021-05-12 08:24:51 +0200

    Reduce memory during msm estimation (#88)

--- src/calculate_moments.py
+++ src/calculate_moments.py
@@ -3,15 +3,40 @@ import numpy as np
 import pandas as pd
 
 
+DEFAULT_WINDOW = 7
+DEFAULT_TAKE_LOGS = True
+DEFAULT_CENTER = False
+DEFAULT_MIN_PERIODS = 1
+
+
 def smoothed_outcome_per_hundred_thousand_sim(
     df,
     outcome,
     groupby=None,
-    window=14,
-    min_periods=1,
-    take_logs=True,
-    center=False,
+    window=DEFAULT_WINDOW,
+    min_periods=DEFAULT_MIN_PERIODS,
+    take_logs=DEFAULT_TAKE_LOGS,
+    center=DEFAULT_CENTER,
 ):
+    """Calculate a daily smoothed outcome on the per 100 000 people level on simulated data.
+
+    Args:
+        df (pandas.DataFrame or dask.dataframe): Simulated time series.
+        outcome (str): Selects a column in df.
+        groupby (list, str or None): Defines the subgroups for which the outcome is
+            calculated.
+        window (int): Over how many days results are averaged to smooth the outcome.
+        min_periods (int): Minimum number of days that need to be present in the
+            smoothing window for the outcome to be not NaN.
+        take_logs (int): Whether the log of the outcome should be returned. If True,
+            smoothing is already done in logs.
+        center (bool): Whether the smoothing window is centered or forward looking.
+
+    Returns:
+        pd.Series: Series with a smoothed outcome. The first index level is date. If
+            groupby is specified, there are additional index levels.
+
+    """
     df = df.reset_index()
     window, min_periods, groupby = _process_inputs(window, min_periods, groupby)
     per_individual = (
@@ -29,15 +54,109 @@ def smoothed_outcome_per_hundred_thousand_sim(
     return out
 
 
+def calculate_period_outcome_sim(df, outcome, groupby=None):
+    """Calculate an outcome on a dataset of one period.
+
+    This uses a groupby over the date column such that the date is preserved as the
+    first index level of the result. Only meant to be used during the msm estimation.
+
+    Args:
+        df (pandas.DataFrame): Simulated states DataFrame for one period.
+        outcome (str): Selects a column in df.
+        groupby (list, str or None): Defines the subgroups for which the outcome is
+            calculated.
+
+    Returns:
+        pd.Series: Series with an unsmoothed outcome for one day. The first index
+            level is date, even though it is meant to be the same for all entries.
+            If groupby is specified, there are additional index levels.
+
+    """
+    if groupby is None:
+        groupby = []
+    elif isinstance(groupby, str):
+        groupby = [groupby]
+
+    out = (
+        df.groupby([pd.Grouper(key="date", freq="D")] + groupby)[outcome]
+        .mean()
+        .fillna(0)
+    )
+
+    if isinstance(df, dd.core.DataFrame):
+        out = out.compute()
+
+    return out
+
+
+def aggregate_and_smooth_period_outcome_sim(
+    simulate_result,
+    outcome,
+    groupby=None,
+    window=DEFAULT_WINDOW,
+    min_periods=DEFAULT_MIN_PERIODS,
+    take_logs=DEFAULT_TAKE_LOGS,
+    center=DEFAULT_CENTER,
+):
+    """Aggregate and smooth a list of per period outcomes in simulate_results.
+
+    Args:
+        simulate_results (dict): Dictionary with a "period_outputs" entry.
+        outcome (str): The name of the outcome in simulate_result["period_outputs"]
+            that should be selected.
+        groupby (list, str or None): Defines the subgroups for which the outcome is
+            calculated.
+        window (int): Over how many days results are averaged to smooth the outcome.
+        min_periods (int): Minimum number of days that need to be present in the
+            smoothing window for the outcome to be not NaN.
+        take_logs (int): Whether the log of the outcome should be returned. If True,
+            smoothing is already done in logs.
+        center (bool): Whether the smoothing window is centered or forward looking.
+
+
+    Returns:
+        pd.Series: Series with a smoothed outcome. The first index level is date. If
+            groupby is specified, there are additional index levels.
+
+    """
+    period_outcomes = simulate_result["period_outputs"][outcome]
+    per_individual = pd.concat(period_outcomes)
+
+    out = _smooth_and_scale_daily_outcome_per_individual(
+        per_individual, window, min_periods, groupby, take_logs, center=center
+    )
+    return out
+
+
 def smoothed_outcome_per_hundred_thousand_rki(
     df,
     outcome,
     groupby=None,
-    window=7,
-    min_periods=1,
+    window=DEFAULT_WINDOW,
+    min_periods=DEFAULT_MIN_PERIODS,
     group_sizes=None,
-    take_logs=True,
+    take_logs=DEFAULT_TAKE_LOGS,
+    center=DEFAULT_CENTER,
 ):
+    """Calculated a smoothed outcome on the per 100 000 people level on empirical data.
+
+    Args:
+        df (pandas.DataFrame): Empirical dataset.
+        outcome (str): Selects a column in df.
+        groupby (list, str or None): Defines the subgroups for which the outcome is
+            calculated.
+        window (int): Over how many days results are averaged to smooth the outcome.
+        min_periods (int): Minimum number of days that need to be present in the
+            smoothing window for the outcome to be not NaN.
+        take_logs (int): Whether the log of the outcome should be returned. If True,
+            smoothing is already done in logs.
+        center (bool): Whether the smoothing window is centered or forward looking.
+
+    Returns:
+        pd.Series: Series with a smoothed outcome. The first index level is date. If
+            groupby is specified, there are additional index levels.
+
+    """
     df = df.reset_index()
     window, min_periods, groupby = _process_inputs(window, min_periods, groupby)
 
@@ -57,7 +176,12 @@ def smoothed_outcome_per_hundred_thousand_rki(
         per_individual = (unstacked / group_sizes).stack()
 
     out = _smooth_and_scale_daily_outcome_per_individual(
-        per_individual, window, min_periods, groupby, take_logs
+        per_individual,
+        window,
+        min_periods,
+        groupby,
+        take_logs,
+        center=center,
     )
     return out
 
@@ -68,7 +192,7 @@ def _smooth_and_scale_daily_outcome_per_individual(
     min_periods,
     groupby,
     take_logs,
-    center=True,
+    center,
 ):
     scaling_factor = 100_000
     scaled = sr * scaling_factor
