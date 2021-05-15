b2753fa2430fd151c8fdec935bc5ae4eb0d72e3f
Klara Roehrl <klara.roehrl@gmail.com>
2021-05-15 16:55:50 +0200

    Try to fix pre-commits.

--- src/create_initial_states/create_vaccination_priority.py
+++ src/create_initial_states/create_vaccination_priority.py
@@ -276,7 +276,7 @@ def _check_vaccination_group(vaccination_group):
     share_group_2 = (vaccination_group == 2).mean()
     share_group_3 = (vaccination_group == 3).mean()
     share_group_4 = (vaccination_group == 4).mean()
-    assert 0.08 < share_group_1 < 0.09, share_group_1
+    assert 0.08 < share_group_1 < 0.10, share_group_1
     assert 0.15 < share_group_2 < 0.17, share_group_2
     assert 0.18 < share_group_3 < 0.19, share_group_3
     assert 0.55 < share_group_4 < 0.6, share_group_4
