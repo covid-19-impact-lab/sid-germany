df08d483e3f20fc7b37b757019d01994e645bab0
Klara Roehrl <klara.roehrl@gmail.com>
2021-05-17 18:20:12 +0200

    Assert that vaccination shares take reasonable values.

--- src/policies/find_people_to_vaccinate.py
+++ src/policies/find_people_to_vaccinate.py
@@ -38,6 +38,13 @@ def find_people_to_vaccinate(
     no_vaccination_share = params.loc[
         ("vaccinations", "share_refuser", "share_refuser"), "value"
     ]
+
+    assert (
+        vaccination_shares < 0.05
+    ).all(), (
+        "Your vaccination shares imply that >=5% of people get vaccinated on one day."
+    )
+
     cutoffs = vaccination_shares.sort_index().cumsum()
     # set all cutoffs before the init_start to 0.
     # that way on the init_start date everyone who should have been vaccinated
