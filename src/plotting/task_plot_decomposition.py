3ac6a105b65f09b5c042ff3c2b830b9e3aa15373
Tobias Raabe <raabe@posteo.de>
2021-06-01 18:46:52 +0200

    Finish plot.

--- src/plotting/task_plot_decomposition.py
+++ src/plotting/task_plot_decomposition.py
@@ -1,9 +1,82 @@
 """This module holds the code to compute marginal contributions and shapley values."""
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import pytask
+
+from src.config import BLD
+from src.config import FAST_FLAG
+from src.scenario_config import create_path_to_scenario_outcome_time_series
+from src.scenario_config import get_available_scenarios
+from src.scenario_config import get_named_scenarios
 
 
-def task_plot_decomposition():
-    pass
+_SCENARIOS = [
+    "spring_baseline",
+    "spring_without_rapid_tests_and_no_vaccinations",
+    "spring_without_vaccinations_without_seasonality",
+    "spring_without_rapid_tests_without_seasonality",
+    "spring_without_rapid_tests",
+    "spring_without_vaccines",
+    "spring_without_seasonality",
+    "spring_no_effects",
+]
+
+
+_AVAILABLE_SCENARIOS = get_available_scenarios(get_named_scenarios())
+
+
+_ARE_ALL_SCENARIOS_AVAILABLE = all(i in _AVAILABLE_SCENARIOS for i in _SCENARIOS)
+
+
+_DEPENDS_ON = {
+    name: create_path_to_scenario_outcome_time_series(name, "newly_infected")
+    for name in _SCENARIOS
+}
+
+
+@pytask.mark.skipif(
+    not _ARE_ALL_SCENARIOS_AVAILABLE, reason="required scenarios are not available"
+)
+@pytask.mark.depends_on(_DEPENDS_ON)
+@pytask.mark.produces(BLD / "figures" / f"{FAST_FLAG}_decomposition.png")
+def task_plot_decomposition(depends_on, produces):
+    scenarios = {name: pd.read_pickle(path) for name, path in depends_on.items()}
+
+    df = {"name": [], "payoff": []}
+    for name, df_ in scenarios.items():
+        df["name"].append(name)
+        df["payoff"].append((df_.mean(axis=1)).sum())
+    df = pd.DataFrame(df)
+
+    df["members"] = [
+        frozenset(["rapid_tests", "vaccinations", "seasonality"]),
+        frozenset(["seasonality"]),
+        frozenset(["rapid_tests"]),
+        frozenset(["vaccinations"]),
+        frozenset(["vaccinations", "seasonality"]),
+        frozenset(["rapid_tests", "seasonality"]),
+        frozenset(["rapid_tests", "vaccinations"]),
+        frozenset([]),
+    ]
+
+    df["payoff"] = df["payoff"] - df["payoff"].max()
+
+    shapley_values = _compute_shapley_values(df)
+
+    ratios = shapley_values / shapley_values.sum()
+    ratios = ratios.rename(
+        index=lambda x: x.replace("shapley_value_", "")
+    ).sort_values()
+
+    fig, ax = plt.subplots()
+
+    ratios.plot(kind="barh", ax=ax)
+
+    ax.set_ylabel("Policy")
+    ax.set_xlabel("Contribution to Reduction")
+
+    plt.savefig(produces)
 
 
 def _compute_shapley_values(coalitions):
