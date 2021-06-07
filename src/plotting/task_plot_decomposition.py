2ea01eeb29ff77f5c06b75a2b9a344b2123bcd9f
Tobias Raabe <raabe@posteo.de>
2021-06-08 00:08:16 +0200

    new plots.

--- src/plotting/task_plot_decomposition.py
+++ src/plotting/task_plot_decomposition.py
@@ -6,21 +6,24 @@ import pytask
 
 from src.config import BLD
 from src.config import FAST_FLAG
+from src.plotting.plotting import format_date_axis
 from src.simulation.scenario_config import create_path_to_scenario_outcome_time_series
 from src.simulation.scenario_config import get_available_scenarios
 from src.simulation.scenario_config import get_named_scenarios
 
 
-_SCENARIOS = [
-    "spring_baseline",
-    "spring_without_rapid_tests_and_no_vaccinations",
-    "spring_without_vaccinations_without_seasonality",
-    "spring_without_rapid_tests_without_seasonality",
-    "spring_without_rapid_tests",
-    "spring_without_vaccines",
-    "spring_without_seasonality",
-    "spring_no_effects",
-]
+_SCENARIO_TO_MEMBERS = {
+    "spring_baseline": frozenset(["rapid_tests", "vaccinations", "seasonality"]),
+    "spring_without_rapid_tests_and_no_vaccinations": frozenset(["seasonality"]),
+    "spring_without_vaccinations_without_seasonality": frozenset(["rapid_tests"]),
+    "spring_without_rapid_tests_without_seasonality": frozenset(["vaccinations"]),
+    "spring_without_rapid_tests": frozenset(["vaccinations", "seasonality"]),
+    "spring_without_vaccines": frozenset(["rapid_tests", "seasonality"]),
+    "spring_without_seasonality": frozenset(["rapid_tests", "vaccinations"]),
+    "spring_no_effects": frozenset([]),
+}
+
+_SCENARIOS = list(_SCENARIO_TO_MEMBERS)
 
 
 _AVAILABLE_SCENARIOS = get_available_scenarios(get_named_scenarios())
@@ -34,51 +37,87 @@ _DEPENDS_ON = {
     for name in _SCENARIOS
 }
 
+MATPLOTLIB_RC_CONTEXT = {
+    "axes.spines.right": False,
+    "axes.spines.top": False,
+    "legend.frameon": False,
+}
+
 
 @pytask.mark.skipif(
     not _ARE_ALL_SCENARIOS_AVAILABLE, reason="required scenarios are not available"
 )
 @pytask.mark.depends_on(_DEPENDS_ON)
-@pytask.mark.produces(BLD / "figures" / f"{FAST_FLAG}_decomposition.pdf")
+@pytask.mark.produces(
+    {
+        "bar_plot": BLD / "figures" / f"{FAST_FLAG}_decomposition_bar.pdf",
+        "area_plot": BLD / "figures" / f"{FAST_FLAG}_decomposition_area.pdf",
+    }
+)
 def task_plot_decomposition(depends_on, produces):
     scenarios = {name: pd.read_pickle(path) for name, path in depends_on.items()}
 
-    df = {"name": [], "payoff": []}
-    for name, df_ in scenarios.items():
-        df["name"].append(name)
-        df["payoff"].append((df_.mean(axis=1)).sum())
-    df = pd.DataFrame(df)
+    df = pd.DataFrame()
+    for name, s in scenarios.items():
+        cumulative_outcomes = s.mean(axis=1)
+        df[name] = cumulative_outcomes
 
-    df["members"] = [
-        frozenset(["rapid_tests", "vaccinations", "seasonality"]),
-        frozenset(["seasonality"]),
-        frozenset(["rapid_tests"]),
-        frozenset(["vaccinations"]),
-        frozenset(["vaccinations", "seasonality"]),
-        frozenset(["rapid_tests", "seasonality"]),
-        frozenset(["rapid_tests", "vaccinations"]),
-        frozenset([]),
-    ]
+    fig = _create_bar_plot(df)
+    fig.savefig(produces["bar_plot"])
 
-    df["payoff"] = df["payoff"] - df["payoff"].max()
+    fig = _create_area_plot(df)
+    fig.savefig(produces["area_plot"])
 
-    shapley_values = _compute_shapley_values(df)
 
-    ratios = (shapley_values / shapley_values.sum()) * 100
+def _create_bar_plot(df):
+    ratios = _compute_shapley_values(df.sum())
     ratios = ratios.rename(
-        index=lambda x: x.replace("shapley_value_", "").replace("_", " ").title()
-    ).sort_values()
-    ratios.index.name = ""
+        index=lambda x: x.replace("shapley_value", "").replace("_", "").title()
+    )
 
-    fig, ax = plt.subplots(figsize=(12, 8))
+    with plt.rc_context(MATPLOTLIB_RC_CONTEXT):
+        fig, ax = plt.subplots()
 
-    ratios.plot(kind="barh", ax=ax)
+        ratios.plot(kind="barh", ax=ax)
 
-    ax.set_xlabel("Contribution to Reduction (in %)")
+        ax.set_xlabel("Contribution to Reduction")
 
-    plt.tight_layout()
+    return fig
 
-    plt.savefig(produces)
+
+def _create_area_plot(df):
+    ratios = df.cumsum().apply(_compute_shapley_values, axis=1)
+    prevented_infections = df["spring_no_effects"] - df["spring_baseline"]
+
+    # Clipping is necessary for the area plot and only small numbers in the beginning
+    # are clipped which do not change the results.
+    prevented_infections_by_channel = (
+        ratios.multiply(prevented_infections.cumsum(), axis=0).diff().clip(0)
+    )
+
+    with plt.rc_context(MATPLOTLIB_RC_CONTEXT):
+        fig, ax = plt.subplots()
+        prevented_infections_by_channel.plot(kind="area", ax=ax)
+
+        ax = format_date_axis(ax)
+
+        ax.set_ylabel("smoothed weekly incidence")
+
+    return fig
+
+
+def _compute_ratios_based_on_shapley_values(s):
+    df = s.to_frame(name="cumulative_infections")
+    df["members"] = df.index.map(_SCENARIO_TO_MEMBERS.get)
+
+    df["payoff"] = (
+        df["cumulative_infections"]
+        - df.loc["spring_no_effects", "cumulative_infections"]
+    ) * -1
+    df = df.reset_index()
+    shapley_values = _compute_shapley_values(df)
+    ratios = shapley_values / shapley_values.sum()
+    return ratios
 
 
 def _compute_shapley_values(coalitions):
