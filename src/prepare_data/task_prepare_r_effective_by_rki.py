e4d22e4e34777f96330c652bffca4e1e60a30122
Klara Röhrl <klara.roehrl@gmail.com>
2021-06-15 13:48:50 +0200

    Fix more rki induced bugs on the R effective and vaccinations (#116)

--- src/prepare_data/task_prepare_r_effective_by_rki.py
+++ src/prepare_data/task_prepare_r_effective_by_rki.py
@@ -5,22 +5,15 @@ import pytask
 from src.config import BLD
 
 
-@pytask.mark.depends_on(BLD / "data" / "raw_time_series" / "reproduction_number.csv")
+@pytask.mark.depends_on(BLD / "data" / "raw_time_series" / "reproduction_number.xlsx")
 @pytask.mark.produces(BLD / "data" / "processed_time_series" / "r_effective.pkl")
 def task_prepare_rki_r_effective_data(depends_on, produces):
-    df = pd.read_csv(depends_on, delimiter=";")
-    df = df[df["Datum"].isnull().cumsum() == 0]
-    df["date"] = pd.to_datetime(df["Datum"], dayfirst=True)
+    df = pd.read_excel(depends_on, sheet_name="Nowcast_R")
+
+    df["date"] = pd.to_datetime(df["Datum des Erkrankungsbeginns"], dayfirst=True)
     df = df.replace(".", np.nan)
-    to_float = [
-        "Schätzer_Reproduktionszahl_R",
-        "UG_PI_Reproduktionszahl_R",
-        "OG_PI_Reproduktionszahl_R",
-        "Schätzer_7_Tage_R_Wert",
-        "UG_PI_7_Tage_R_Wert",
-        "OG_PI_7_Tage_R_Wert",
-    ]
-    for col in to_float:
-        df[col] = df[col].str.replace(",", ".").astype(float)
     df = df.set_index("date").sort_index()
-    df.to_pickle(produces)
+
+    r_effective = df["Punktschätzer des 7-Tage-R Wertes"]
+    r_effective = r_effective.str.replace(",", ".").astype(float)
+    r_effective.to_pickle(produces)
