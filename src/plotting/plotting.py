f00241485dfafefc8f1fb41a9ffb4abff421110e
Klara Roehrl <klara.roehrl@gmail.com>
2021-06-17 10:43:43 +0200

    Fix that all plots have the same size.

--- src/plotting/plotting.py
+++ src/plotting/plotting.py
@@ -107,10 +107,7 @@ def plot_incidences(
 
     """
     if fig is None and ax is None:
-        if len(incidences) <= 4:
-            fig, ax = plt.subplots(figsize=PLOT_SIZE)
-        else:
-            fig, ax = plt.subplots(figsize=(8, 6))
+        fig, ax = plt.subplots(figsize=PLOT_SIZE)
 
     if colors is None:
         colors = [BLUE, ORANGE, RED, TEAL, GREEN, YELLOW, PURPLE, BROWN]
