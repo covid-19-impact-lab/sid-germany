7e2368ec23273a61eb7a48433ff8e045e47f301f
Klara Roehrl <klara.roehrl@gmail.com>
2021-07-07 14:32:18 +0200

    Temp. Working on tests.

--- tests/test_create_rapid_test_statistics.py
+++ tests/test_create_rapid_test_statistics.py
@@ -3,46 +3,9 @@ import pandas as pd
 
 from src.config import POPULATION_GERMANY
 from src.testing.create_rapid_test_statistics import (
-    _calculate_true_positive_and_false_negatives,
+    _calculate_rapid_test_statistics_by_channel,
 )
-from src.testing.create_rapid_test_statistics import _calculate_weights
-from src.testing.create_rapid_test_statistics import _share_demanded_by_infected
 from src.testing.create_rapid_test_statistics import create_rapid_test_statistics
-
-
-def test_share_demanded_by_infected():
-    # 1st: not infected. does not demand test -> should be ignored
-    # 2nd: not infected and demands a test -> enters denominator
-    # 3rd: infected and does not demand a test -> should be ignored
-    # 4th and 5th: infected and demand a test -> enter numerator and denominator
-    states = pd.DataFrame({"currently_infected": [False, False, True, True, True]})
-    weights = pd.DataFrame({"channel": [0, 0.5, 0.2, 1, 0.5]})
-    demand_by_channel = pd.DataFrame({"channel": [False, True, False, True, True]})
-    res = _share_demanded_by_infected(
-        demand_by_channel=demand_by_channel,
-        states=states,
-        weights=weights,
-        channel="channel",
-    )
-    expected = 1.5 / (0.5 + 1.5)
-    assert res == expected
-
-
-def test_calculate_weights():
-    demand_by_channel = pd.DataFrame(
-        {
-            "a": [False, False, True, True],
-            "b": [False, True, False, True],
-        }
-    )
-    expected = pd.DataFrame(
-        {
-            "a": [0, 0, 1, 0.5],
-            "b": [0, 1, 0, 0.5],
-        }
-    )
-    res = _calculate_weights(demand_by_channel)
-    assert expected.equals(res)
 
 
 def test_create_rapid_test_statistics(monkeypatch):
@@ -76,10 +39,6 @@ def test_create_rapid_test_statistics(monkeypatch):
         params=None,
     )
 
-    # weights:
-    # a: 0, 0, 1, 0.5, 0, 0, 1, 0.5
-    # b: 0, 1, 0, 0.5, 0, 1, 0, 0.5
-    #
     # groups:
     # a: 2, 3, 6, 7
     # b: 1, 3, 5, 7
@@ -96,43 +55,62 @@ def test_create_rapid_test_statistics(monkeypatch):
     # false negative: 3, 7
     # false positive: 6
 
+    scaling = POPULATION_GERMANY / len(states)
     expected = pd.DataFrame(
         {
             0: {
                 "date": date,
-                "n_individuals": 8,
-                "share_with_rapid_test_through_a": 3 / 8,
-                "share_of_a_rapid_tests_demanded_by_infected": 2 / 3,
-                "share_with_rapid_test_through_b": 3 / 8,
-                "share_of_b_rapid_tests_demanded_by_infected": 1 / 3,
-                "share_with_rapid_test": 0.75,
-                "n_rapid_tests_overall_in_germany": int(0.75 * POPULATION_GERMANY),
-                "n_rapid_tests_through_a": 3,
-                "n_rapid_tests_through_b": 3,
-                "false_positive_rate_in_the_population": 1 / 8,
                 # overall shares
-                "true_positive_rate_overall": 0.5,
-                "true_negative_rate_overall": 0.5,
-                "false_negative_rate_overall": 0.5,
-                "false_positive_rate_overall": 0.5,
+                "true_positive_rate_by_overall": 0.5,
+                "true_negative_rate_by_overall": 0.5,
+                "false_negative_rate_by_overall": 0.5,
+                "false_positive_rate_by_overall": 0.5,
                 # shares in a
-                "true_positive_rate_in_a": 0.5,
-                "true_negative_rate_in_a": 0.0,
-                "false_negative_rate_in_a": 1.0,
-                "false_positive_rate_in_a": 0.5,
+                "true_positive_rate_by_a": 0.5,
+                "true_negative_rate_by_a": 0.0,
+                "false_negative_rate_by_a": 1.0,
+                "false_positive_rate_by_a": 0.5,
                 # shares in b
-                "true_positive_rate_in_b": np.nan,
-                "true_negative_rate_in_b": 0.5,
-                "false_negative_rate_in_b": 0.5,
-                "false_positive_rate_in_b": np.nan,
+                "true_positive_rate_by_b": np.nan,
+                "true_negative_rate_by_b": 0.5,
+                "false_negative_rate_by_b": 0.5,
+                "false_positive_rate_by_b": np.nan,
+                # numbers
+                "number_false_negative_by_a": 2 * scaling,
+                "number_false_negative_by_b": 2 * scaling,
+                "number_false_negative_by_overall": 2 * scaling,
+                "number_true_negative_by_a": 0.0,
+                "number_true_negative_by_b": 2 * scaling,
+                "number_true_negative_by_overall": 2 * scaling,
+                "number_false_positive_by_a": 1 * scaling,
+                "number_false_positive_by_b": np.nan,
+                "number_false_positive_by_overall": 1 * scaling,
+                "number_true_positive_by_a": 1 * scaling,
+                "number_true_positive_by_b": 0.0,
+                "number_true_positive_by_overall": 1 * scaling,
+                #
+                "number_tested_by_a": 4 * scaling,
+                "number_tested_by_b": 4 * scaling,
+                "number_tested_by_overall": 6 * scaling,
+                "number_tested_negative_by_a": 2 * scaling,
+                "number_tested_negative_by_b": 4 * scaling,
+                "number_tested_negative_by_overall": 4 * scaling,
+                "number_tested_positive_by_a": 2 * scaling,
+                "number_tested_positive_by_b": 0 * scaling,
+                "number_tested_positive_by_overall": 2 * scaling,
             }
         }
     )
-    assert set(expected.index) == set(res.index)
-    pd.testing.assert_frame_equal(expected.loc[res.index], res)
+
+    expected.columns = [1]
+    df = pd.concat([res, expected], axis=1)
+    diff = df[df[0] != df[1]].dropna()
+
+    ### pd.testing.assert_frame_equal(expected, res.loc[expected.index])
+    ### assert set(expected.index) == set(res.index)
 
 
-def test_calculate_true_positive_and_false_negatives():
+def test_calculate_rapid_test_statistics_by_channel():
     # 1: True positive
     # 2: True negative
     # 3: False positive
@@ -146,16 +124,22 @@ def test_calculate_true_positive_and_false_negatives():
     rapid_test_results = pd.Series([True, False, True, False, False, False])
     receives_rapid_test = pd.Series([True, True, True, True, True, False])
 
-    (
-        res_share_true_positive,
-        res_share_false_negative,
-        res_false_positive_rate_in_the_population,
-    ) = _calculate_true_positive_and_false_negatives(
+    res = _calculate_rapid_test_statistics_by_channel(
         states=states,
         rapid_test_results=rapid_test_results,
         receives_rapid_test=receives_rapid_test,
+        channel_name="channel",
     )
 
-    assert res_share_true_positive == 1 / 2  # 1 and 3 tested positive, 1 is infected
-    assert res_share_false_negative == 2 / 3  # 2,4,5 tested negative, 4, 5 infected
-    assert res_false_positive_rate_in_the_population == 1 / 6
+    scaling = POPULATION_GERMANY / len(states)
+    expected = {
+        "number_tested_by_channel": 5 * scaling,
+        "number_tested_positive_by_channel": 2 * scaling,
+        "number_tested_negative_by_channel": 3 * scaling,
+        "number_tested_false_positive_by_channel": 1 * scaling,
+        "number_tested_false_negative_by_channel": 2 * scaling,
+        "number_tested_true_positive_by_channel": 1 * scaling,
+        "number_tested_true_negative_by_channel": 1 * scaling,
+    }
+
+    assert res == expected
