73d329d3e846c71a00392afa273519645d2d2430
Klara Röhrl <klara.roehrl@gmail.com>
2021-11-08 14:54:31 +0100

    Only add delta (#126)
    * Add RKI data on Delta and prepare the empirical Delta shares.
    
    * Introduce Delta with twice the infectiousness of alpha.
    
    * Adjust msm_criterion to include delta.
    
    * Plot the full length of the simulated B117 time series.
    
    * Plot share of delta as part of the scenario comparison plots.
    
    * Scale up the infection probs by factor 2.0 as well.
    
    * Add delta share to the non incidence outcomes.

--- src/create_initial_states/task_build_full_params.py
+++ src/create_initial_states/task_build_full_params.py
@@ -183,6 +183,16 @@ def _add_virus_strain_params(params):
     params = params.copy(deep=True)
     params.loc[("virus_strain", "base_strain", "factor"), "value"] = 1.0
     params.loc[("virus_strain", "b117", "factor"), "value"] = 1.67
+
+    # for Delta:
+    # - CDC
+    #   (https://www.cdc.gov/coronavirus/2019-ncov/variants/delta-variant.html):
+    #   "more than 2x as contagious as previous variants." (Sep 2)
+    # - meta-analysis (10.1093/jtm/taab124):
+    #   basic replication number estimated from 3.2 to 8.
+    # - BBC (https://www.bbc.com/news/health-57431420):
+    #   basic replication number from 5-8 vs. 4-5 for Alpha.
+    params.loc[("virus_strain", "delta", "factor"), "value"] = 3.33  # 2.0 * 1.67
     return params
 
 
@@ -423,4 +433,8 @@ def _add_event_params(params):
     params = params.copy(deep=True)
     params.loc[("events", "b117_cases_per_100_000", "2021-01-01"), "value"] = 0
     params.loc[("events", "b117_cases_per_100_000", "2021-01-31"), "value"] = 0.986
+
+    params.loc[("events", "delta_cases_per_100_000", "2021-04-15"), "value"] = 0
+    params.loc[("events", "delta_cases_per_100_000", "2021-06-01"), "value"] = 0.06
+
     return params
